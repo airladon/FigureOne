35f72c795b762354fc19b235713eb7ad58e60902
airladon <airladon@gmail.com>
2018-12-28 09:20:29 -0800

    add side label testing

--- src/js/diagram/DiagramObjects/PolyLine.test.js
+++ src/js/diagram/DiagramObjects/PolyLine.test.js
@@ -51,6 +51,41 @@ describe('Diagram Objects PolyLine', () => {
           text: 'a',
         },
       }),
+      SideLabelsOpen: () => diagram.objects.polyLine({
+        points,
+        close: false,
+        sideLabel: {
+          text: 'a',
+        },
+      }),
+      SideLabelsFullDefine: () => diagram.objects.polyLine({
+        points,
+        close: true,
+        sideLabel: {
+          text: ['a', 'b', 'c'],
+        },
+      }),
+      SideLabelsUnderDefine: () => diagram.objects.polyLine({
+        points,
+        close: true,
+        sideLabel: {
+          text: ['a', 'b'],
+        },
+      }),
+      SideLabelsOverDefine: () => diagram.objects.polyLine({
+        points,
+        close: true,
+        sideLabel: {
+          text: ['a', 'b', 'c', 'd'],
+        },
+      }),
+      SideLabelsSingleDefine: () => diagram.objects.polyLine({
+        points,
+        close: true,
+        sideLabel: {
+          text: 'a',
+        },
+      }),
     };
   });
   test('Number of points in line close', () => {
@@ -65,16 +100,64 @@ describe('Diagram Objects PolyLine', () => {
     expect(poly._line.drawingObject.points).toHaveLength((points.length - 1) * 12);
     expect(Object.keys(poly.elements)).toHaveLength(1);
   });
-  test.only('Side Labels Close', () => {
-    const poly = ways.SideLabelsClose();
-    expect(Object.keys(poly.elements)).toHaveLength(4);
-    expect(Object.keys(poly.elements))
-      .toEqual(['line', 'side01', 'side12', 'side20']);
-    expect(poly.elements.side01.p1).toEqual(points[0]);
-    expect(poly.elements.side01.p2).toEqual(points[1]);
-    expect(poly.elements.side12.p1).toEqual(points[1]);
-    expect(poly.elements.side12.p2).toEqual(points[2]);
-    expect(poly.elements.side20.p1).toEqual(points[2]);
-    expect(poly.elements.side20.p2.round()).toEqual(points[0]);
+  describe('Side Labels', () => {
+    test('Close', () => {
+      const poly = ways.SideLabelsClose();
+      expect(Object.keys(poly.elements)).toHaveLength(4);
+      expect(Object.keys(poly.elements))
+        .toEqual(['line', 'side01', 'side12', 'side20']);
+      expect(poly.elements.side01.p1).toEqual(points[0]);
+      expect(poly.elements.side01.p2).toEqual(points[1]);
+      expect(poly.elements.side12.p1).toEqual(points[1]);
+      expect(poly.elements.side12.p2).toEqual(points[2]);
+      expect(poly.elements.side20.p1).toEqual(points[2]);
+      expect(poly.elements.side20.p2.round()).toEqual(points[0]);
+    });
+    test('Open', () => {
+      const poly = ways.SideLabelsOpen();
+      expect(Object.keys(poly.elements)).toHaveLength(3);
+      expect(Object.keys(poly.elements))
+        .toEqual(['line', 'side01', 'side12']);
+      expect(poly.elements.side01.p1).toEqual(points[0]);
+      expect(poly.elements.side01.p2).toEqual(points[1]);
+      expect(poly.elements.side12.p1).toEqual(points[1]);
+      expect(poly.elements.side12.p2).toEqual(points[2]);
+    });
+    test('Full Define', () => {
+      const poly = ways.SideLabelsFullDefine();
+      const base01 = poly.elements.side01.label.eqn.collection._base;
+      const base12 = poly.elements.side12.label.eqn.collection._base;
+      const base20 = poly.elements.side20.label.eqn.collection._base;
+      expect(base01.drawingObject.text[0].text).toBe('a');
+      expect(base12.drawingObject.text[0].text).toBe('b');
+      expect(base20.drawingObject.text[0].text).toBe('c');
+    });
+    test('Under Define', () => {
+      const poly = ways.SideLabelsUnderDefine();
+      const base01 = poly.elements.side01.label.eqn.collection._base;
+      const base12 = poly.elements.side12.label.eqn.collection._base;
+      const base20 = poly.elements.side20.label.eqn.collection._base;
+      expect(base01.drawingObject.text[0].text).toBe('a');
+      expect(base12.drawingObject.text[0].text).toBe('b');
+      expect(base20.drawingObject.text[0].text).toBe('a');
+    });
+    test('Over Define', () => {
+      const poly = ways.SideLabelsOverDefine();
+      const base01 = poly.elements.side01.label.eqn.collection._base;
+      const base12 = poly.elements.side12.label.eqn.collection._base;
+      const base20 = poly.elements.side20.label.eqn.collection._base;
+      expect(base01.drawingObject.text[0].text).toBe('a');
+      expect(base12.drawingObject.text[0].text).toBe('b');
+      expect(base20.drawingObject.text[0].text).toBe('c');
+    });
+    test('Single Define', () => {
+      const poly = ways.SideLabelsSingleDefine();
+      const base01 = poly.elements.side01.label.eqn.collection._base;
+      const base12 = poly.elements.side12.label.eqn.collection._base;
+      const base20 = poly.elements.side20.label.eqn.collection._base;
+      expect(base01.drawingObject.text[0].text).toBe('a');
+      expect(base12.drawingObject.text[0].text).toBe('a');
+      expect(base20.drawingObject.text[0].text).toBe('a');
+    });
   });
 });
