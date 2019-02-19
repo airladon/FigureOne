3d4dab3678138bd8eade8b3152c2c972620042c2
airladon <airladon@gmail.com>
2019-02-19 12:03:44 -0800

    basic animator working

--- src/js/diagram/Animation/AnimationStep/SerialAnimationStep.js
+++ src/js/diagram/Animation/AnimationStep/SerialAnimationStep.js
@@ -10,7 +10,7 @@ export type TypeSerialAnimationStepInputOptions = {
 } & TypeAnimationStepInputOptions;
 
 export default class SerialAnimationStep extends AnimationStep {
-  animationSteps: Array<AnimationStep>;
+  steps: Array<AnimationStep>;
   index: number;
 
   constructor(optionsIn: TypeSerialAnimationStepInputOptions) {
@@ -18,24 +18,24 @@ export default class SerialAnimationStep extends AnimationStep {
     this.index = 0;
     const defaultOptions = {};
     const options = joinObjects({}, defaultOptions, optionsIn);
-    this.animationSteps = [];
-    if (!Array.isArray(options.steps)) {
-      this.animationSteps = [options.steps];
+    this.steps = [];
+    if (!Array.isArray(options.steps) && options.steps != null) {
+      this.steps = [options.steps];
     } else if (options.steps != null) {
-      this.animationSteps = options.steps;
+      this.steps = options.steps;
     }
     return this;
   }
 
   then(step: AnimationStep) {
-    this.animationSteps.push(step);
+    this.steps.push(step);
     return this;
   }
 
   startWaiting() {
     super.startWaiting();
-    this.animationSteps.forEach((animationStep) => {
-      animationStep.startWaiting();
+    this.steps.forEach((step) => {
+      step.startWaiting();
     });
   }
 
@@ -43,21 +43,21 @@ export default class SerialAnimationStep extends AnimationStep {
     this.startWaiting();
     super.start();
     this.index = 0;
-    if (this.animationSteps.length > 0) {
-      this.animationSteps[0].start();
+    if (this.steps.length > 0) {
+      this.steps[0].start();
     }
   }
 
   nextFrame(now: number) {
-    const remaining = this.animationSteps[this.index].nextFrame(now);
+    const remaining = this.steps[this.index].nextFrame(now);
     if (remaining > 0) {
-      if (this.index === this.animationSteps.length - 1) {
+      if (this.index === this.steps.length - 1) {
         this.finish();
         return remaining;
       }
       this.index += 1;
-      this.animationSteps[this.index].start();
-      this.animationSteps[this.index].startTime = now - remaining;
+      this.steps[this.index].start();
+      this.steps[this.index].startTime = now - remaining;
       this.nextFrame(now);
     }
     return 0;
@@ -68,9 +68,9 @@ export default class SerialAnimationStep extends AnimationStep {
       return;
     }
     super.finish(cancelled, force);
-    this.animationSteps.forEach((animationStep) => {
-      if (animationStep.state !== 'idle') {
-        animationStep.finish(cancelled, force);
+    this.steps.forEach((step) => {
+      if (step.state !== 'idle') {
+        step.finish(cancelled, force);
       }
     });
     if (this.onFinish != null) {
