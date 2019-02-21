0ddeb1e81d8b41a7d55a48d608fda26bde71047e
“airladon” <airladon@gmail.com>
2019-02-20 16:02:04 -0800

    AnimationManager initial tests

--- src/js/diagram/Animation/AnimationManager.test.js
+++ src/js/diagram/Animation/AnimationManager.test.js
@@ -21,6 +21,7 @@ describe('Animation Manager', () => {
     const diagram = makeDiagram();
     elem = diagram.objects.line();
     elem.setPosition(new Point(0, 0));
+    elem.setColor([1, 0, 0, 1]);
     p1 = new Point(1, 1);
   });
   test('Basic', () => {
@@ -35,17 +36,34 @@ describe('Animation Manager', () => {
 
     elem.anim.nextFrame(1.1);
     expect(elem.getPosition().round()).toEqual(point(1));
+    expect(elem.anim.sequences).toHaveLength(0);
   });
-  // test('Duplicate', () => {
-  //   const animator = new Animator({ element });
-  //   const p1 = new Point(1, 1);
-  //   const p2 = new Point(2, 2);
-  //   animator
-  //     .moveTo({ target: p1, duration: 1, progression: 'linear' })
-  //     .moveTo({ target: p2, duration: 1, progression: 'linear' });
-  //   const dup = animator._dup();
-  //   expect(dup).not.toBe(animator);
-  //   expect(dup).toEqual(animator);
-  //   expect(dup.element).toBe(animator.element);
-  // });
+  test('Two parallel animations', () => {
+    elem.anim.new()
+      .moveTo({ target: p1, duration: 1, progression: 'linear' })
+      .start();
+    elem.anim.new()
+      .dissolveOut(2)
+      .start();
+    elem.anim.nextFrame(0);
+    expect(elem.getPosition().round()).toEqual(point(0));
+    expect(math.round(elem.color, 2)).toEqual([1, 0, 0, 1]);
+
+    elem.anim.nextFrame(0.5);
+    expect(elem.getPosition().round()).toEqual(point(0.5));
+    expect(math.round(elem.color, 2)).toEqual([1, 0, 0, 0.75]);
+    expect(elem.isShown).toBe(true);
+    expect(elem.anim.sequences).toHaveLength(2);
+
+    elem.anim.nextFrame(1.5);
+    expect(elem.getPosition().round()).toEqual(point(1));
+    expect(math.round(elem.color, 2)).toEqual([1, 0, 0, 0.25]);
+    expect(elem.anim.sequences).toHaveLength(1);
+
+    elem.anim.nextFrame(2.1);
+    expect(elem.getPosition().round()).toEqual(point(1));
+    expect(math.round(elem.color, 2)).toEqual([1, 0, 0, 1]);
+    expect(elem.isShown).toBe(false);
+    expect(elem.anim.sequences).toHaveLength(0);
+  });
 });
