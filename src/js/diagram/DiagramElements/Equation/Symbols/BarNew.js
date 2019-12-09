ca946f031667098dad8ef1624d0c9af68caf2fb7
airladon <airladon@gmail.com>
2019-12-09 07:17:26 -0800

    Added bar

---
+++ src/js/diagram/DiagramElements/Equation/Symbols/BarNew.js
@@ -0,0 +1,36 @@
+// @flow
+import {
+  Point,
+} from '../../../../tools/g2';
+import Bracket from './BracketNew';
+
+
+export default class Bar extends Bracket {
+  // eslint-disable-next-line class-methods-use-this
+  getWidth() {
+    return (type: 'static' | 'dynamic', options: Object, height: number) => {
+      const { lineWidth } = options;
+      if (type === 'static') {
+        return height * lineWidth;
+      }
+      return lineWidth;
+    };
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getPoints() {
+    return (options: Object, height: number) => {
+      const { lineWidth } = options;
+
+      const leftPoints = [
+        new Point(0, 0),
+        new Point(0, height),
+      ];
+      const rightPoints = [
+        new Point(lineWidth, 0),
+        new Point(lineWidth, height),
+      ];
+      return [leftPoints, rightPoints, lineWidth, height];
+    }
+  }
+}
