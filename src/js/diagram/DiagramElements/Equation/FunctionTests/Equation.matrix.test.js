d88585e6b7d4082e3f46681cadfdd220d42262b6
airladon <airladon@gmail.com>
2019-12-18 21:27:28 -0800

    add matrix tests

--- src/js/diagram/DiagramElements/Equation/FunctionTests/Equation.matrix.test.js
+++ src/js/diagram/DiagramElements/Equation/FunctionTests/Equation.matrix.test.js
@@ -1,6 +1,6 @@
-// import {
-//   Point,
-// } from '../../../../tools/g2';
+import {
+  Point,
+} from '../../../../tools/g2';
 import {
   round,
 } from '../../../../tools/math';
@@ -50,6 +50,7 @@ describe('Equation Functions - Matrix', () => {
                 scale: 1,
                 fit: 'min',
                 space: [0.1, 0.1],
+                vAlign: 'baseline',
                 brac: {
                   inSize: null,
                   insideSpace: 0.1,
@@ -74,6 +75,7 @@ describe('Equation Functions - Matrix', () => {
               scale: 1,
               fit: 'min',
               space: [0.1, 0.1],
+              vAlign: 'baseline',
               brac: {
                 inSize: null,
                 insideSpace: 0.1,
@@ -90,7 +92,7 @@ describe('Equation Functions - Matrix', () => {
           // Method Array
           2: { matrix: [
             [2, 2], 'left', ['a', 'b', 'c', 'd'], 'right', 1, 'min',
-            [0.1, 0.1], {
+            [0.1, 0.1], 'baseline', {
               inSize: null,
               insideSpace: 0.1,
               outsideSpace: 0.1,
@@ -104,7 +106,7 @@ describe('Equation Functions - Matrix', () => {
           // Function with Method Array
           3: e.matrix([
             [2, 2], 'left', ['a', 'b', 'c', 'd'], 'right', 1, 'min',
-            [0.1, 0.1], {
+            [0.1, 0.1], 'baseline', {
               inSize: null,
               insideSpace: 0.1,
               outsideSpace: 0.1,
@@ -124,6 +126,7 @@ describe('Equation Functions - Matrix', () => {
             scale: 1,
             fit: 'min',
             space: [0.1, 0.1],
+            vAlign: 'baseline',
             brac: {
               inSize: null,
               insideSpace: 0.1,
@@ -155,15 +158,51 @@ describe('Equation Functions - Matrix', () => {
                 scale: 1,
                 fit: 'min',
                 space: [0.1, 0.1],
+                vAlign: 'baseline',
                 brac: { insideSpace: 0.1 },
               },
             },
             scale: 1,
           },
-          order: {
+          rowVector: {
             content: matrix([
               [1, 4], 'left', ['a', 'b', 'c', 'd'], 'right',
-              1, 'min', [0.1, 0.1], { insideSpace: 0.1 },
+              1, 'min', [0.1, 0.1], 'baseline', { insideSpace: 0.1 },
+            ]),
+            scale: 1,
+          },
+          columnVector: {
+            content: matrix([
+              [4, 1], 'left', ['a', 'b', 'c', 'd'], 'right',
+              1, 'min', [0.1, 0.1], 'baseline', { insideSpace: 0.1 },
+            ]),
+            scale: 1,
+          },
+          scale: {
+            content: matrix([
+              [2, 2], 'left', ['a', 'b', 'c', 'd'], 'right',
+              0.5, 'min', [0.1, 0.1], 'baseline', { insideSpace: 0.1 },
+            ]),
+            scale: 1,
+          },
+          fit: {
+            content: matrix([
+              [2, 2], 'left', ['a', 'b', 'c', 'd'], 'right',
+              1, 'max', [0.1, 0.1], 'baseline', { insideSpace: 0.1 },
+            ]),
+            scale: 1,
+          },
+          space: {
+            content: matrix([
+              [2, 2], 'left', ['a', 'b', 'c', 'd'], 'right',
+              1, 'min', [0.2, 0.3], 'baseline', { insideSpace: 0.1 },
+            ]),
+            scale: 1,
+          },
+          vAlign: {
+            content: matrix([
+              [2, 2], 'left', ['a', 'b', 'c', 'd'], 'right',
+              1, 'min', [0.1, 0.1], 'middle', { insideSpace: 0.1 },
             ]),
             scale: 1,
           },
@@ -179,9 +218,10 @@ describe('Equation Functions - Matrix', () => {
     let baseC;
     let baseD;
     let baseLeft;
-    let left;
     let initialSpace;
     let insideSpace;
+    let scale;
+    let space;
     beforeEach(() => {
       functions.parameterSteps();
       eqn.showForm('base');
@@ -193,10 +233,11 @@ describe('Equation Functions - Matrix', () => {
       baseLeft = eqn._left.getBoundingRect('diagram');
       initialSpace = 0.1;
       insideSpace = 0.1;
+      scale = 0.5;
+      space = new Point(0.2, 0.3);
     });
-    test('order', () => {
-      // console.log(baseA)
-      eqn.showForm('order');
+    test('Row Vector', () => {
+      eqn.showForm('rowVector');
       diagram.setFirstTransform();
       const newA = eqn._a.getBoundingRect('diagram');
       const newB = eqn._b.getBoundingRect('diagram');
@@ -206,6 +247,63 @@ describe('Equation Functions - Matrix', () => {
       expect(round(newB.left)).toBe(round(newA.right + initialSpace));
       expect(round(newC.left)).toBe(round(newB.right + initialSpace));
       expect(round(newD.left)).toBe(round(newC.right + initialSpace));
+      expect(round(newB.bottom)).toBe(round(newA.bottom));
+      expect(round(newC.bottom)).toBe(round(newA.bottom));
+      expect(round(newD.bottom)).toBe(round(newA.bottom));
+    });
+    test('Column Vector', () => {
+      eqn.showForm('columnVector');
+      diagram.setFirstTransform();
+      const newA = eqn._a.getBoundingRect('diagram');
+      const newB = eqn._b.getBoundingRect('diagram');
+      const newC = eqn._c.getBoundingRect('diagram');
+      const newD = eqn._d.getBoundingRect('diagram');
+      expect(round(newA.left)).toBe(round(baseLeft.right + insideSpace));
+      expect(round(newB.left)).toBe(round(newA.left));
+      expect(round(newC.left)).toBe(round(newB.left));
+      expect(round(newD.left)).toBe(round(newC.left));
+      expect(round(newA.bottom)).toBe(round(newB.top + initialSpace));
+      expect(round(newB.bottom)).toBe(round(newC.top + initialSpace));
+      expect(round(newC.bottom)).toBe(round(newD.top + initialSpace));
+    });
+    test('Scale', () => {
+      eqn.showForm('scale');
+      diagram.setFirstTransform();
+      const newA = eqn._a.getBoundingRect('diagram');
+      const newB = eqn._b.getBoundingRect('diagram');
+      const newC = eqn._c.getBoundingRect('diagram');
+      const newD = eqn._d.getBoundingRect('diagram');
+      expect(round(newA.height)).toBe(round(baseA.height * scale));
+      expect(round(newB.height)).toBe(round(baseB.height * scale));
+      expect(round(newC.height)).toBe(round(baseC.height * scale));
+      expect(round(newD.height)).toBe(round(baseD.height * scale));
+    });
+    test('max', () => {
+      eqn.showForm('fit');
+      diagram.setFirstTransform();
+      // in this case the height of the b or d is the max dimension so the width
+      // of all columns becomes it.
+      const newA = eqn._a.getBoundingRect('diagram');
+      const newB = eqn._b.getBoundingRect('diagram');
+      expect(round(newA.left))
+        .toBe(round(baseLeft.right + initialSpace + (newB.height - newA.width) / 2));
+    });
+    test('Space', () => {
+      eqn.showForm('space');
+      diagram.setFirstTransform();
+      const newA = eqn._a.getBoundingRect('diagram');
+      const newB = eqn._b.getBoundingRect('diagram');
+      const newD = eqn._d.getBoundingRect('diagram');
+      expect(round(newB.left)).toBe(round(newA.right + space.x));
+      expect(round(newB.bottom)).toBe(round(newD.top + space.y));
+    });
+    test('vAlign', () => {
+      eqn.showForm('vAlign');
+      diagram.setFirstTransform();
+      const newA = eqn._a.getBoundingRect('diagram');
+      const newB = eqn._b.getBoundingRect('diagram');
+      expect(round(newA.bottom))
+        .toBe(round(newB.bottom + (newB.height - newA.height) / 2));
     });
   });
   test('Input Forms', () => {
