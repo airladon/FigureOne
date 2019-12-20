b08e5402beab12eddd63f745a76184fa0fb3f2a4
airladon <airladon@gmail.com>
2019-12-19 16:13:24 -0800

    fix square bracket tests

--- src/js/diagram/DiagramElements/Equation/Symbols/SquareBracket.js
+++ src/js/diagram/DiagramElements/Equation/Symbols/SquareBracket.js
@@ -7,22 +7,22 @@ import Bracket from './Bracket';
 
 export default class SquareBracket extends Bracket {
   // eslint-disable-next-line class-methods-use-this
-  getWidth() {
-    return (type: 'static' | 'dynamic', options: Object, height: number) => {
-      const { width } = options;
-      if (type === 'static') {
-        return height * width;
-      }
-      return width;
-    };
-  }
+  // getWidth() {
+  //   return (type: 'static' | 'dynamic', options: Object, height: number) => {
+  //     const { width } = options;
+  //     if (type === 'static') {
+  //       return height * width;
+  //     }
+  //     return width;
+  //   };
+  // }
 
   //                            width
   //                  |<--------------------->|
   //
   //            ___   ._______________________    ____
   //           A      |                       |      A
-  //           |      |                       |      | line end width
+  //           |      |                       |      | tipWidth
   //           |      |      .________________|   ___V
   //           |      |      |
   //           |      |      |
@@ -47,10 +47,16 @@ export default class SquareBracket extends Bracket {
   //
   // eslint-disable-next-line class-methods-use-this
   getPoints() {
-    return (options: Object, height: number) => {
+    return (options: Object, widthIn: number, height: number) => {
+      // const {
+      //   lineWidth, width, endLineWidth, radius, sides,
+      // } = options;
+
       const {
-        lineWidth, width, endLineWidth, radius, sides,
+        sides, side, radius,
       } = options;
+
+      const { lineWidth, width, tipWidth } = this.getDefaultValues(height, widthIn, options);
 
       if (radius === 0) {
         const outsidePoints = [
@@ -60,12 +66,12 @@ export default class SquareBracket extends Bracket {
           new Point(width, height),
         ];
         const insidePoints = [
-          new Point(width, endLineWidth),
-          new Point(lineWidth, endLineWidth),
-          new Point(lineWidth, height - endLineWidth),
-          new Point(width, height - endLineWidth),
+          new Point(width, tipWidth),
+          new Point(lineWidth, tipWidth),
+          new Point(lineWidth, height - tipWidth),
+          new Point(width, height - tipWidth),
         ];
-        return [outsidePoints, insidePoints, width, height];
+        return this.getBracketPoints(outsidePoints, insidePoints, side, width, height);
       }
 
       const radiusToUse = Math.min(radius, width, height / 2);
@@ -92,7 +98,39 @@ export default class SquareBracket extends Bracket {
 
       outsidePoints.push(new Point(width, height));
       insidePoints.push(new Point(width, height - lineWidth));
-      return [outsidePoints, insidePoints, width, height];
+      return this.getBracketPoints(outsidePoints, insidePoints, side, width, height);
     };
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getDefaultValues(height: number, width: ?number, options: {
+      lineWidth?: number,
+      width?: number,
+      tipWidth?: number,
+    }) {
+    const out = {};
+    if (width == null && options.width == null) {
+      out.width = (97570.78 + (0.004958708 - 97570.78)
+                  / (1 + (height / 2399858) ** 0.9383909)) * 0.8;
+    }
+    if (width != null) {
+      out.width = width;
+    }
+    if (options.width != null) {
+      out.width = options.width;
+    }
+    if (options.lineWidth == null) {
+      out.lineWidth = (0.2933614 + (0.0001418178 - 0.2933614)
+                      / (1 + (height / 39.01413) ** 0.618041)) * 0.8;
+    } else {
+      out.lineWidth = options.lineWidth;
+    }
+
+    if (options.tipWidth == null) {
+      out.tipWidth = out.lineWidth * 0.7;
+    } else {
+      out.tipWidth = options.tipWidth;
+    }
+    return out;
   }
 }
--- src/js/diagram/DiagramElements/Equation/Symbols/SquareBracket.test.js
+++ src/js/diagram/DiagramElements/Equation/Symbols/SquareBracket.test.js
@@ -20,13 +20,13 @@ describe('Equation Symbols - Square Bracket', () => {
   let color1;
   let elements;
   let lineWidth;
-  let endLineWidth;
+  let tipWidth;
   let width;
   let height;
   beforeEach(() => {
     width = 0.3;
     lineWidth = 0.1;
-    endLineWidth = 0.05;
+    tipWidth = 0.05;
     height = 2;
     diagram = makeDiagram();
     color1 = [0.95, 0, 0, 1];
@@ -36,17 +36,18 @@ describe('Equation Symbols - Square Bracket', () => {
         symbol: 'squareBracket',
         side: 'left',
         lineWidth,
-        endLineWidth,
+        tipWidth,
         width,
-        staticSize: false,
+        draw: 'dynamic',
+        radius: 0,
       },
       right: {
         symbol: 'squareBracket',
         side: 'right',
         lineWidth: 0.01,
-        endLineWidth: 0.01,
+        tipWidth: 0.01,
         width: 0.03,
-        staticSize: true,
+        draw: 'dynamic',
       },
       leftCurved: {
         symbol: 'squareBracket',
@@ -55,7 +56,8 @@ describe('Equation Symbols - Square Bracket', () => {
         width: 0.03,
         radius: 0.03,
         sides: 10,
-        staticSize: true,
+        draw: 'static',
+        staticHeight: 'first',
       },
     };
     eqn = new EquationNew(diagram.shapes, { color: color1 });
@@ -84,6 +86,7 @@ describe('Equation Symbols - Square Bracket', () => {
     // Snapshot test on most simple layout
     eqn.showForm('0');
     diagram.setFirstTransform();
+    // console.log(eqn._left.drawingObject.points)
     expect(round(eqn._left.drawingObject.points)).toMatchSnapshot();
     expect(round(eqn._right.drawingObject.points)).toMatchSnapshot();
   });
@@ -121,12 +124,12 @@ describe('Equation Symbols - Square Bracket', () => {
       allPoints.push(new Point(points[i], points[i + 1]));
     }
     expect(allPoints[0].isEqualTo(new Point(width, 0))).toBe(true);
-    expect(allPoints[1].isEqualTo(new Point(width, endLineWidth))).toBe(true);
+    expect(allPoints[1].isEqualTo(new Point(width, tipWidth))).toBe(true);
     expect(allPoints[2].isEqualTo(new Point(0, 0))).toBe(true);
-    expect(allPoints[3].isEqualTo(new Point(lineWidth, endLineWidth))).toBe(true);
+    expect(allPoints[3].isEqualTo(new Point(lineWidth, tipWidth))).toBe(true);
     expect(allPoints[4].isEqualTo(new Point(0, height))).toBe(true);
-    expect(allPoints[5].isEqualTo(new Point(lineWidth, height - endLineWidth))).toBe(true);
+    expect(allPoints[5].isEqualTo(new Point(lineWidth, height - tipWidth))).toBe(true);
     expect(allPoints[6].isEqualTo(new Point(width, height))).toBe(true);
-    expect(allPoints[7].isEqualTo(new Point(width, height - endLineWidth))).toBe(true);
+    expect(allPoints[7].isEqualTo(new Point(width, height - tipWidth))).toBe(true);
   });
 });
