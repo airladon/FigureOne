69a38192aea15ba73b01d65c47c804f6b3398a78
airladon <airladon@gmail.com>
2020-03-10 15:27:40 -0700

    update docs

--- generate_docs.sh
+++ generate_docs.sh
@@ -3,3 +3,7 @@ documentation build src/index.js -f html -o ./docs --config documentation.yml
 
 cat docs/assets/style.css docs/custom.css > docs/assets/style1.css
 mv docs/assets/style1.css docs/assets/style.css
+
+documentation build src/index.js -f md -o ./docs/README.md --config documentation.yml
+
+# documentation build src/index.js -f html -o ./docs --config documentation.yml
