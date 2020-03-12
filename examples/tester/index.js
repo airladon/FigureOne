548e221854430ef30de2b8c7d5aa68cc46cf476c
airladon <airladon@gmail.com>
2020-03-11 17:52:47 -0700

    add thickenLIne

--- examples/tester/index.js
+++ examples/tester/index.js
@@ -1,28 +1,29 @@
 // Create diagram
 const diagram = new Fig.Diagram();
 const { Point } = Fig;
-const { thickenCorner } = Fig.tools.g2;
+const { thickenCorner, thickenLine } = Fig.tools.g2;
 
 const line = [
-  new Point(1, 0),
+  new Point(0, 1),
   new Point(0, 0),
-  new Point(0, 1),
-];
-const thickLine = [
   new Point(1, 0),
-  new Point(1, 0.1),
-  ...thickenCorner(line[0], line[1], line[2], 0.1),
-  new Point(0, 1),
-  new Point(0.1, 1),
 ];
-console.log(thickLine)
+// const thickLine = [
+//   new Point(1, 0),
+//   new Point(1, 0.1),
+//   ...thickenCorner(line[0], line[1], line[2], 0.1),
+//   new Point(0, 1),
+//   new Point(0.1, 1),
+// ];
+
+// console.log(thickLine)
 // Add elements to the diagram
 diagram.addElement(
   {
     name: 'r',
     method: 'shapes.generic',
     options: {
-      points: thickLine,
+      points: thickenLine(line, 0.1, true),
       drawType: 'strip',
     },
   },
