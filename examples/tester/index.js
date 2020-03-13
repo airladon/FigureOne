65bb13f198d987aca61954df2eea5db997023439
airladon <airladon@gmail.com>
2020-03-13 10:35:47 -0700

    line basically functionaly

--- examples/tester/index.js
+++ examples/tester/index.js
@@ -13,9 +13,10 @@ const line = [
   new Point(1.5, 0),
   new Point(0.75, 0.2),
   new Point(0, 0),
+  new Point(0.9, 0.5),
 ];
 
-const makeLine = (lineIn) => makePolyLine(lineIn, 0.02, true, 'outside', 'auto', 0.005, 10, Math.PI / 7, [0.2, 0.05, 0.01, 0.05])
+const makeLine = (lineIn) => makePolyLine(lineIn, 0.02, true, 'mid', 'auto', 0.005, 10, Math.PI / 7); // , [0.2, 0.05, 0.01, 0.05])
 const thick = makeLine(line);
 
 console.log(thick)
