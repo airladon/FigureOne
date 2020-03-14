435286eb86c918a5787b2a1fcb0612043a514271
airladon <airladon@gmail.com>
2020-03-13 22:07:29 -0700

    udpate polyline docs

--- examples/tester/index.js
+++ examples/tester/index.js
@@ -24,79 +24,94 @@ diagram.addElements([
     name: 'p',
     method: 'shapes.polyLine',
     options: {
-      points: [[0, 0], [0.5, 0], [0.5, 0.5], [0, 0.5]],
-      width: 0.01,
-      color: [0, 1, 0, 1],
-      dash: [0.03, 0.01, 0.01, 0.01],
+      points: [[-0.5, -0.5], [0.5, -0.5], [0.5, 0.5], [-0.5, 0.5]],
+      width: 0.05,
+      dash: [0.17, 0.05, 0.05, 0.05],
       close: true,
       cornerStyle: 'radius',
       cornerSize: 0.1,
-      pointsAt: 'outside',
     },
   },
-  {
-    name: 'p1',
-    method: 'shapes.polyLine',
-    options: {
-      points: [[0, 0], [0.5, 0], [0.5, 0.5]],
-      width: 0.005,
-      color: [0, 1, 1, 1],
-      // dash: [0.01, 0.01],
-      close: true,
-      cornerStyle: 'radius',
-      cornerSize: 0.1,
-      pointsAt: 'mid',
-    },
-  },
-  {
-    name: 'pad',
-    method: 'polygon',
-    options: {
-      radius: 0.2,
-      color: [0.5, 0.5, 0.5, 0.5],
-      sides: 100,
-      fill: true,
-    },
-  },
-  {
-    name: 'r',
-    method: 'shapes.generic',
-    options: {
-      points: thick,
-      drawType: 'triangles',
-      position: [-0.7, -0.5],
-    },
-  },
-  {
-    name: 'x',
-    method: 'line',
-    options: {
-      p1: [-1, -0.4],
-      p2: [1, -0.4],
-      width: 0.005,
-      color: [0.5, 0.5, 0.5, 0.5],
-    }
-  },
-  {
-    name: 'x2',
-    method: 'line',
-    options: {
-      p1: [-1, -0.5],
-      p2: [1, -0.5],
-      width: 0.005,
-      color: [0.5, 0.5, 0.5, 0.5],
-    }
-  },
-  {
-    name: 'x3',
-    method: 'line',
-    options: {
-      p1: [-1, -0.55],
-      p2: [1, -0.55],
-      width: 0.005,
-      color: [0.5, 0.5, 0.5, 0.5],
-    }
-  },
+  // {
+  //   name: 'p',
+  //   method: 'shapes.polyLine',
+  //   options: {
+  //     points: [[-0.5, -0.5], [0.5, -0.5], [0, 0.5]],
+  //     width: 0.05,
+  //     close: true,
+  //   },
+  // },
+  // {
+  //   name: 'p',
+  //   method: 'shapes.polyLine',
+  //   options: {
+  //     points: [[-0.5, -0.5], [-0.1, 0.5], [0.3, -0.2], [0.5, 0.5]],
+  //     width: 0.05,
+  //   },
+  // },
+  // {
+  //   name: 'p1',
+  //   method: 'shapes.polyLine',
+  //   options: {
+  //     points: [[0, 0], [0.5, 0], [0.5, 0.5]],
+  //     width: 0.005,
+  //     color: [0, 1, 1, 1],
+  //     // dash: [0.01, 0.01],
+  //     close: true,
+  //     cornerStyle: 'radius',
+  //     cornerSize: 0.1,
+  //     pointsAt: 'mid',
+  //   },
+  // },
+  // {
+  //   name: 'pad',
+  //   method: 'polygon',
+  //   options: {
+  //     radius: 0.2,
+  //     color: [0.5, 0.5, 0.5, 0.5],
+  //     sides: 100,
+  //     fill: true,
+  //   },
+  // },
+  // {
+  //   name: 'r',
+  //   method: 'shapes.generic',
+  //   options: {
+  //     points: thick,
+  //     drawType: 'triangles',
+  //     position: [-0.7, -0.5],
+  //   },
+  // },
+  // {
+  //   name: 'x',
+  //   method: 'line',
+  //   options: {
+  //     p1: [-1, -0.4],
+  //     p2: [1, -0.4],
+  //     width: 0.005,
+  //     color: [0.5, 0.5, 0.5, 0.5],
+  //   }
+  // },
+  // {
+  //   name: 'x2',
+  //   method: 'line',
+  //   options: {
+  //     p1: [-1, -0.5],
+  //     p2: [1, -0.5],
+  //     width: 0.005,
+  //     color: [0.5, 0.5, 0.5, 0.5],
+  //   }
+  // },
+  // {
+  //   name: 'x3',
+  //   method: 'line',
+  //   options: {
+  //     p1: [-1, -0.55],
+  //     p2: [1, -0.55],
+  //     width: 0.005,
+  //     color: [0.5, 0.5, 0.5, 0.5],
+  //   }
+  // },
 ]);
 
 // Show the equation form
