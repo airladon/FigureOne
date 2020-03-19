371b3bf4cb32dda636befa1648de673842516391
airladon <airladon@gmail.com>
2020-03-19 14:46:30 -0700

    update polyline qpi for positive and negative

--- examples/tester/index.js
+++ examples/tester/index.js
@@ -8,53 +8,55 @@ const line = [
     // new Point(0, 1),
 ];
 
-// diagram.addElements([
-//   {
-//     name: 'pad',
-//     method: 'polygon',
-//     options: {
-//       radius: 0.2,
-//       color: [0.5, 0.5, 0.5, 0.5],
-//       sides: 100,
-//       fill: true,
-//     },
-//   },
-//   {
-//     name: 'r',
-//     method: 'shapes.polyline',
-//     options: {
-//       points: line,
-//       width: 0.04,
-//       close: true,
-//       // fill: false,
-//       cornersOnly: true,
-//       cornerLength: 0.1,
-//       // forceCornerLength: true,
-//       // asdfasdf: false,
-//       pointsAt: 'mid',
-//       // dash: [0.1, 0.03],
-//     },
-//   },
-//   {
-//     name: 'x2',
-//     method: 'line',
-//     options: {
-//       p1: [-1, 0],
-//       p2: [1, 0],
-//       width: 0.005,
-//       color: [0.5, 0.5, 0.5, 0.5],
-//     }
-//   },
-// ]);
+diagram.addElements([
+  {
+    name: 'pad',
+    method: 'polygon',
+    options: {
+      radius: 0.2,
+      color: [0.5, 0.5, 0.5, 0.5],
+      sides: 100,
+      fill: true,
+    },
+  },
+  {
+    name: 'r',
+    method: 'shapes.polyline',
+    options: {
+      points: line,
+      width: 0.04,
+      close: true,
+      // fill: false,
+      // cornersOnly: true,
+      // cornerLength: 0.1,
+      // forceCornerLength: true,
+      // asdfasdf: false,
+      pointsAt: 'negative',
+      // dash: [0.1, 0.03],
+    },
+  },
+  {
+    name: 'x2',
+    method: 'line',
+    options: {
+      p1: [-1, 0],
+      p2: [1, 0],
+      width: 0.005,
+      color: [0.5, 0.5, 0.5, 0.5],
+    }
+  },
+]);
 
-// const pad = diagram.getElement('pad');
-// pad.setMovable();
-// pad.setTransformCallback = () => {
-//   line[1] = pad.getPosition();
-//   const r = diagram.getElement('r');
-//   r.custom.updatePoints(line);
-//   diagram.animateNextFrame();
-// }
+const pad = diagram.getElement('pad');
+pad.setMovable();
+pad.setTransformCallback = () => {
+  line[1] = pad.getPosition();
+  const r = diagram.getElement('r');
+  r.custom.updatePoints(line);
+  diagram.animateNextFrame();
+}
+diagram.initialize();
+pad.setPosition(0, 0.5);
 
 // const img = new Image();
 // img.crossOrigin = "anonymous";
@@ -71,17 +73,17 @@ const line = [
 // console.log(img.src)
 // diagram.initialize();
 // pad.setPosition(0, 0.5);
-diagram.addElement(
-  {
-    name: 'p',
-    method: 'polygon',
-    options: {
-      sides: 6,
-      width: 0.05,
-      radius: 0.9,
-      fill: true,
-      position: [0, 0],
-      textureLocation: 'example.png',
-    },
-  },
-);
+// diagram.addElement(
+//   {
+//     name: 'p',
+//     method: 'polygon',
+//     options: {
+//       sides: 6,
+//       width: 0.05,
+//       radius: 0.9,
+//       fill: true,
+//       position: [0, 0],
+//       // textureLocation: 'example.png',
+//     },
+//   },
+// );
