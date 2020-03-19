6c4fab3e514d7770cd7c13ccdc12715de16c6324
airladon <airladon@gmail.com>
2020-03-19 10:06:46 -0700

    adding textures to generic

--- examples/8 - Texture/index.js
+++ examples/8 - Texture/index.js
@@ -1,24 +1,40 @@
 const diagram = new Fig.Diagram();
-const { Point } = Fig;
+const { Point, Rect } = Fig;
 
 const line = [
-    new Point(0.5, 0),
-    new Point(0, 0.5),
-    new Point(-0.5, 0),
+    // new Point(0.5, 0),
+    // new Point(0, 0.5),
+    // new Point(-0.5, 0),
     // new Point(0, 1),
+    new Point(-1, -1),
+    new Point(-1, 1),
+    new Point(1, 1),
+    new Point(1, -1),
 ];
 
 diagram.addElement(
+  // {
+  //   name: 'p',
+  //   method: 'polygon',
+  //   options: {
+  //     sides: 8,
+  //     width: 0.05,
+  //     radius: 0.9,
+  //     fill: true,
+  //     position: [0, 0],
+  //     textureLocation: 'http://localhost:8000/texture.jpg',
+  //   },
+  // },
   {
-    name: 'p',
-    method: 'polygon',
+    name: 'p1',
+    method: 'shapes.polyline',
     options: {
-      sides: 8,
-      width: 0.05,
-      radius: 0.9,
-      fill: true,
-      position: [0, 0],
+      points: line,
+      width: 0.9,
+      pointsAt: 'outside',
+      close: true,
       textureLocation: 'http://localhost:8000/texture.jpg',
+      textureVertexSpace: new Rect(-0.5, -0.5, 1, 1),
     },
   },
 );
