700fc17e11d3034185c83056f985944268d62a19
airladon <airladon@gmail.com>
2020-03-26 11:06:00 -0700

    updates

--- examples/tester/index.js
+++ examples/tester/index.js
@@ -79,46 +79,74 @@ const line = [
 // diagram.initialize();
 // pad.setPosition(-0.7, 0);
 
-diagram.addElements([
+// diagram.addElements([
+//   {
+//     name: 'p',
+//     method: 'shapes.polygon',
+//     options: {
+//       sides: 3,
+//       width: 0.004,
+//       radius: 0.1,
+//       line: {
+//         widthIs: 'mid',
+//         cornerStyle: 'radius',
+//         cornerSize: 0.02,
+//         cornerSides: 5,
+//         dash: [0.02, 0.005, 0.005, 0.005],
+//       },
+//       copy: [
+//         { point: [0, 1], from: 0, tostep: 1 }
+//         'a',
+//         { transform: }
+//         { linear: { }, from: 0, to: 1 }
+//         { angle: { }, : false }
+//         { arc: { }, cum: true }
+//       copy: [
+//         { linear: { num: 5, }}
+//       ]
+//         new Copy()
+//           .point()
+//           .points()
+//           .transform()
+//           .transforms()
+//           .mark()
+//           .angle()
+//           .linear()
+//           .arc({ fromMark: 'a', toMark: 'b',})
+//       ]
+//       copyChain: [
+//         { num: 2, step: 0.3, angle: 0 },
+//         { numAngle: 20, step: Math.PI * 2 / 18, skip: 1/3 }
+//       ],
+//     },
+//   },
+// ]);
+
+diagram.addElement(
+  // Add equation element
   {
-    name: 'p',
-    method: 'shapes.polygon',
+    name: 'eqn',
+    method: 'equation',
     options: {
-      sides: 3,
-      width: 0.004,
-      radius: 0.1,
-      line: {
-        widthIs: 'mid',
-        cornerStyle: 'radius',
-        cornerSize: 0.02,
-        cornerSides: 5,
-        dash: [0.02, 0.005, 0.005, 0.005],
+      color: [0.95, 0.95, 0.6, 1],
+      position: [-0.2, 0],
+      // Equation elements are the individual terms in the equation
+      elements: {
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        v: { symbol: 'vinculum'},
+        equals: ' = ',
       },
-      copy: [
-        { point: [0, 1], from: 0, tostep: 1 }
-        'a',
-        { transform: }
-        { linear: { }, from: 0, to: 1 }
-        { angle: { }, : false }
-        { arc: { }, cum: true }
-      copy: [
-        { linear: { num: 5, }}
-      ]
-        new Copy()
-          .point()
-          .points()
-          .transform()
-          .transforms()
-          .mark()
-          .angle()
-          .linear()
-          .arc({ fromMark: 'a', toMark: 'b',})
-      ]
-      copyChain: [
-        { num: 2, step: 0.3, angle: 0 },
-        { numAngle: 20, step: Math.PI * 2 / 18, skip: 1/3 }
-      ],
+      // An equation form is how those terms are arranged
+      forms: {
+        base: ['a', 'equals', { frac: ['b', 'vinculum', 'c'] }],
+      },
     },
   },
-]);
+);
+
+// Show the equation form
+diagram.getElement('eqn').showForm('base');
+
 diagram.initialize();
