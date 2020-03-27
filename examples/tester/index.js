e2456291281881e446ba17926c0c2dc7ec3d908c
airladon <airladon@gmail.com>
2020-03-26 21:40:23 -0700

    add copy to generic

--- examples/tester/index.js
+++ examples/tester/index.js
@@ -79,74 +79,81 @@ const line = [
 // diagram.initialize();
 // pad.setPosition(-0.7, 0);
 
-// diagram.addElements([
-//   {
-//     name: 'p',
-//     method: 'shapes.polygon',
-//     options: {
-//       sides: 3,
-//       width: 0.004,
-//       radius: 0.1,
-//       line: {
-//         widthIs: 'mid',
-//         cornerStyle: 'radius',
-//         cornerSize: 0.02,
-//         cornerSides: 5,
-//         dash: [0.02, 0.005, 0.005, 0.005],
-//       },
-//       copy: [
-//         { point: [0, 1], from: 0, tostep: 1 }
-//         'a',
-//         { transform: }
-//         { linear: { }, from: 0, to: 1 }
-//         { angle: { }, : false }
-//         { arc: { }, cum: true }
-//       copy: [
-//         { linear: { num: 5, }}
-//       ]
-//         new Copy()
-//           .point()
-//           .points()
-//           .transform()
-//           .transforms()
-//           .mark()
-//           .angle()
-//           .linear()
-//           .arc({ fromMark: 'a', toMark: 'b',})
-//       ]
-//       copyChain: [
-//         { num: 2, step: 0.3, angle: 0 },
-//         { numAngle: 20, step: Math.PI * 2 / 18, skip: 1/3 }
-//       ],
-//     },
-//   },
-// ]);
-
-diagram.addElement(
-  // Add equation element
+diagram.addElements([
   {
-    name: 'eqn',
-    method: 'equation',
+    name: 'p',
+    method: 'shapes.polygon',
     options: {
-      color: [0.95, 0.95, 0.6, 1],
-      position: [-0.2, 0],
-      // Equation elements are the individual terms in the equation
-      elements: {
-        a: 'a',
-        b: 'b',
-        c: 'c',
-        v: { symbol: 'vinculum'},
-        equals: ' = ',
+      sides: 4,
+      width: 0.004,
+      radius: 0.1,
+      rotation: Math.PI / 4,
+      line: {
+        widthIs: 'mid',
+        cornerStyle: 'radius',
+        cornerSize: 0.02,
+        cornerSides: 5,
+        dash: [0.02, 0.005, 0.005, 0.005],
       },
-      // An equation form is how those terms are arranged
-      forms: {
-        base: ['a', 'equals', { frac: ['b', 'vinculum', 'c'] }],
-      },
+      offset: [-0.5, -0.5],
+      copyChain: [
+        { linear: { num: 5, step: 0.2 } },
+        { linear: { num: 5, step: 0.2, axis: 'y' } },
+        // { angle: { num: 5, step: Math.PI / 3, start: 1 } },
+      ],
+      // copy: [
+      //   { point: [0, 1], from: 0, tostep: 1 }
+      //   'a',
+      //   { transform: }
+      //   { linear: { }, from: 0, to: 1 }
+      //   { angle: { }, : false }
+      //   { arc: { }, cum: true }
+      // copy: [
+      //   { linear: { num: 5, }}
+      // ]
+      //   new Copy()
+      //     .point()
+      //     .points()
+      //     .transform()
+      //     .transforms()
+      //     .mark()
+      //     .angle()
+      //     .linear()
+      //     .arc({ fromMark: 'a', toMark: 'b',})
+      // ]
+      // copyChain: [
+      //   { num: 2, step: 0.3, angle: 0 },
+      //   { numAngle: 20, step: Math.PI * 2 / 18, skip: 1/3 }
+      // ],
     },
   },
-);
+]);
+
+// diagram.addElement(
+//   // Add equation element
+//   {
+//     name: 'eqn',
+//     method: 'equation',
+//     options: {
+//       color: [0.95, 0.95, 0.6, 1],
+//       position: [-0.2, 0],
+//       // Equation elements are the individual terms in the equation
+//       elements: {
+//         a: 'a',
+//         b: 'b',
+//         c: 'c',
+//         v: { symbol: 'vinculum'},
+//         equals: ' = ',
+//       },
+//       // An equation form is how those terms are arranged
+//       forms: {
+//         base: ['a', 'equals', { frac: ['b', 'vinculum', 'c'] }],
+//       },
+//     },
+//   },
+// );
 
 // Show the equation form
-diagram.getElement('eqn').showForm('base');
+// diagram.getElement('eqn').showForm('base');
 
 diagram.initialize();
--- src/js/diagram/DrawingObjects/Geometries/copy/copy.js
+++ src/js/diagram/DrawingObjects/Geometries/copy/copy.js
@@ -30,15 +30,15 @@ type CPY_Angle = {
   center?: number,
 } & CPY_Generic;
 
-type CPY_Arc = {
-  arcStep?: number,
-  angleStep?: number,
-  startAngle?: number,
-  stopAngle?: number,
-  step?: number,
-  num?: number,
-  center?: TypeParsablePoint,
-}
+// type CPY_Arc = {
+//   arcStep?: number,
+//   angleStep?: number,
+//   startAngle?: number,
+//   stopAngle?: number,
+//   step?: number,
+//   num?: number,
+//   center?: TypeParsablePoint,
+// }
 
 // type CPY_Location = TypeParsablePoint | Array<TypeParsablePoint> | {
 //   to?: TypeParsablePoint | Array<TypeParsablePoint>,
@@ -55,45 +55,11 @@ type CPY_Transform = Transform | Array<Transform> | {
 type CPY_Step = { linear: CPY_Linear }
               | { angle: CPY_Angle }
               | { offset: CPY_Offset }
-              | { arc: CPY_Arc }
+              // | { arc: CPY_Arc }
               | { transform: CPY_Transform };
 
 type CPY_Steps = Array<CPY_Step>;
 
-
-// function angleCopy(
-//   points: Array<Point>,
-//   copyIn: TypeCopyAngle,
-// ) {
-//   const defaultCopy = {
-//     numAngle: 1,
-//     step: Math.PI / 4,
-//     center: [0, 0],
-//     skip: 0,
-//   };
-
-//   const copy = joinObjects({}, defaultCopy, copyIn);
-//   copy.center = getPoint(copy.center);
-//   const out = [];
-//   const skipPoints = Math.floor(copy.skip ? points.length * copy.skip : 0);
-
-//   for (let i = 0; i < copy.numAngle + 1; i += 1) {
-//     const matrix = new Transform()
-//       .translate(-copy.center.x, -copy.center.y)
-//       .rotate(i * copy.step)
-//       .translate(copy.center.x, copy.center.y)
-//       .matrix();
-//     const startPoint = i === 0 ? 0 : skipPoints;
-//     for (let p = startPoint; p < points.length; p += 1) {
-//       out.push(points[p].transformBy(matrix));
-//     }
-//   }
-//   return out;
-// }
-
-// Array<Transform> | Array<Point> | TypeCopyAngle
-// | TypeCopyLinear | Point | Transform | TypeCopyOffset
-// | TypeCopyTransform;
 type CPY_Marks = { [markName: string]: number };
 
 function getPointsToCopy(
@@ -235,161 +201,64 @@ function copyAngle(
   return out;
 }
 
-function copyArc(
-  pointsToCopy: Array<Point>,
-  initialPoints: Array<Point>,
-  optionsIn: CPY_Arc,
-) {
-  const defaultOptions = {
-    num: 1,
-    step: 0.1,
-    angleStep: Math.PI / 4,
-    // arcStep: 0.1,
-    startAngle: 0,
-    stopAngle: Math.PI * 2,
-    center: [0, 0],
-  };
+// function copyArc(
+//   pointsToCopy: Array<Point>,
+//   initialPoints: Array<Point>,
+//   optionsIn: CPY_Arc,
+// ) {
+//   const defaultOptions = {
+//     num: 1,
+//     step: 0.1,
+//     angleStep: Math.PI / 4,
+//     // arcStep: 0.1,
+//     startAngle: 0,
+//     stopAngle: Math.PI * 2,
+//     center: [0, 0],
+//   };
 
-  const options = joinObjects({}, defaultOptions, optionsIn);
-  options.center = getPoint(options.center);
+//   const options = joinObjects({}, defaultOptions, optionsIn);
+//   options.center = getPoint(options.center);
 
-  let out = initialPoints;
-  const { center } = options;
-  for (let i = 1; i < options.num + 1; i += 1) {
-    const matrix = new Transform()
-      .translate(-center.x, -center.y)
-      .rotate(i * options.step)
-      .translate(center.x, center.y)
-      .matrix();
-    out = [...out, ...pointsToCopy.map(p => p.transformBy(matrix))];
-  }
-  return out;
-}
-
+//   let out = initialPoints;
+//   const { center } = options;
+//   for (let i = 1; i < options.num + 1; i += 1) {
+//     const matrix = new Transform()
+//       .translate(-center.x, -center.y)
+//       .rotate(i * options.step)
+//       .translate(center.x, center.y)
+//       .matrix();
+//     out = [...out, ...pointsToCopy.map(p => p.transformBy(matrix))];
+//   }
+//   return out;
+// }
 
 
 function copyStep(
   points: Array<Point>,
   copyStyle: 'linear' | 'offset' | 'arc' | 'angle' | 'transform',
-  copy: CPY_Linear | CPY_Offset,
+  options: CPY_Linear | CPY_Offset,
   // marks: CPY_Marks,
 ) {
-  // const options = {
-  //   start: 0,
-  //   end: 'end',
-  //   original: true,
-  // };
-  // if (copy.start != null) {
-  //   options.start = copy.start;
-  // }
-  // if (copy.end != null) {
-  //   options.end = copy.end;
-  // }
-  // if (copy.original != null) {
-  //   options.original = copy.original;
-  // }
-
-  // const pointsToCopy = getPointsToCopy(points, options.start, options.end, marks);
-
-  // let out = [];
-  // if (options.original) {
-  //   out = [...points];
-  // }
   const out = [];
+  // if (copyStyle === 'move') {
+  //   return move(points, out, options);
+  // }
   if (copyStyle === 'offset') {
-    return copyOffset(points, out, copy);
+    return copyOffset(points, out, options);
   }
 
   if (copyStyle === 'linear') {
-    return copyLinear(points, out, copy);
+    return copyLinear(points, out, options);
   }
 
   if (copyStyle === 'transform') {
-    return copyTransform(points, out, copy);
+    return copyTransform(points, out, options);
   }
 
   if (copyStyle === 'angle') {
-    return copyAngle(points, out, copy);
+    return copyAngle(points, out, options);
   }
   return points;
-  // if (copy instanceof Point) {
-  //   return [
-  //     ...points.map(p => p._dup()),
-  //     ...points.map(p => p.add(copy)),
-  //   ];
-  // }
-  // if (copy instanceof Transform) {
-  //   const matrix = copy.matrix();
-  //   return [
-  //     ...points.map(p => p._dup()),
-  //     ...points.map(p => p.transformBy(matrix)),
-  //   ];
-  // }
-  // if (Array.isArray(copy) && copy.length === 0) {
-  //   return points.map(p => p._dup());
-  // }
-  // if ((Array.isArray(copy) && typeof copy[0] === 'number')
-  //     || typeof copy === 'number') {
-  //   return [
-  //     ...points.map(p => p._dup()),
-  //     ...points.map(p => p.add(getPoint(copy))),
-  //   ];
-  // }
-  // if (Array.isArray(copy)) {
-  //   const firstElement = copy[0];
-  //   let out = [];
-  //   out = points.map(p => p._dup());
-  //   if (firstElement instanceof Point) {
-  //     copy.forEach((copyPoint) => {
-  //       out = [...out, ...points.map(p => p.add(copyPoint))];
-  //     });
-  //     return out;
-  //   }
-  //   if (firstElement instanceof Transform) {
-  //     copy.forEach((copyTransform) => {
-  //       const matrix = copyTransform.matrix();
-  //       out = [...out, ...points.map(p => p.transformBy(matrix))];
-  //     });
-  //     return out;
-  //   }
-  //   if (Array.isArray(firstElement)) {
-  //     copy.forEach((copyPoint) => {
-  //       const cp = getPoint(copyPoint);
-  //       out = [...out, ...points.map(p => p.add(cp))];
-  //     });
-  //     return out;
-  //   }
-  //   if (typeof firstElement === 'number') {
-  //     copy.forEach((copyPoint) => {
-  //       const cp = getPoint(copyPoint);
-  //       out = [...out, ...points.map(p => p.add(cp))];
-  //     });
-  //     return out;
-  //   }
-  // }
-
-  // if (!Array.isArray(copy) && copy.offset != null) {
-  //   if (copy.offset instanceof Transform) {
-  //     const matrix = copy.offset.matrix();
-  //     return points.map(p => p.transformBy(matrix));
-  //   }
-  //   const offset = getPoint(copy.offset);
-  //   return points.map(p => p.add(getPoint(offset)));
-  // }
-
-  // // if (!Array.isArray(copy) && copy.transform != null) {
-  // //   const matrix = copy.transform.matrix();
-  // //   return points.map(p => p.transformBy(matrix));
-  // // }
-
-  // if (!Array.isArray(copy) && copy.num != null) {
-  //   return linearCopy(points, copy);
-  // }
-
-  // if (!Array.isArray(copy) && copy.numAngle != null) {
-  //   return angleCopy(points, copy);
-  // }
-  // return points;
 }
 
 function copyPoints(
