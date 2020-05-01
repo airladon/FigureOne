4dc96b6e145cc4d5759c6f30d0cc7df594425bd2
airladon <airladon@gmail.com>
2020-04-30 17:43:31 -0700

    seek and cursor basically working

--- src/js/diagram/Diagram.js
+++ src/js/diagram/Diagram.js
@@ -1237,7 +1237,6 @@ class Diagram {
       return;
     }
 
-    console.log('isMoving', this.elements.isMoving());
     if (this.elements.isMoving()) {
       this.animateNextFrame(true, 'is moving');
     }
