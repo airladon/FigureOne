0a0235fd332a60f4185eaea8baeeedbb7463489e
airladon <airladon@gmail.com>
2020-05-23 11:14:12 -0700

    passing big data

--- src/js/diagram/recorder.worker.js
+++ src/js/diagram/recorder.worker.js
@@ -1,10 +1,6 @@
 // @flow
 
 addEventListener("message", (event) => {
-  // const [a, b] = event;
-  // console.log('in worker')
-  // // Do stuff with the message
-  // postMessage(a + b);
   console.log('worker', event.data);
-  postMessage(`From Worker: ${event.data[0] + 2 * event.data[1]}`)
+  // postMessage(`From Worker: ${event.data[0] + 2 * event.data[1]}`)
 });
