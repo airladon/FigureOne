12c95dfef03ced49ecbc2e5434df68d7b6bec6a8
airladon <airladon@gmail.com>
2020-06-19 15:59:56 -0700

    ongoing

--- src/js/diagram/Animation/AnimationStep/ElementAnimationStep/ScenarioAnimationStep.test.js
+++ src/js/diagram/Animation/AnimationStep/ElementAnimationStep/ScenarioAnimationStep.test.js
@@ -269,8 +269,95 @@ describe('Diagram Recorder', () => {
     expect(a.opacity).toEqual(1);
     expect(diagram.isAnimating()).toBe(false);
   });
-  test('Velocity all same duration', () => {});
-  test('Velocity different durations', () => {});
+  test('Velocity all same duration', () => {
+    a.animations.new()
+      .scenario({
+        target: {
+          isShown: false,
+          color: [0, 1, 0, 1],
+          position: [1, 1],
+          scale: 2,
+          rotation: 1,
+        },
+        velocity: {
+          position: 0.1,
+          scale: 0.2,
+          rotation: 0.3,
+          color: 0.4,
+          opacity: 0.5,
+        },
+        allSameDuration: true,
+      })
+      .start();
+
+    diagram.mock.timeStep(0);
+    expect(a.getPosition()).toEqual(new Point(0, 0));
+    expect(a.getScale()).toEqual(new Point(1, 1));
+    expect(a.getRotation()).toBe(0);
+    expect(a.opacity).toBe(1);
+    expect(a.isShown).toBe(true);
+    expect(a.color).toEqual([1, 0, 0, 1]);
+    diagram.mock.timeStep(5);
+    expect(a.getPosition()).toEqual(new Point(0.5, 0.5));
+    expect(a.getScale()).toEqual(new Point(1.5, 1.5));
+    expect(a.getRotation()).toBe(0.5);
+    expect(a.opacity).toBe(0.5005);
+    expect(a.isShown).toBe(true);
+    expect(a.color).toEqual([0.5, 0.5, 0, 1]);
+    diagram.mock.timeStep(10);
+    expect(a.getPosition()).toEqual(new Point(1, 1));
+    expect(a.getScale()).toEqual(new Point(2, 2));
+    expect(a.getRotation()).toBe(1);
+    expect(a.opacity).toBe(1);
+    expect(a.isShown).toBe(false);
+    expect(a.color).toEqual([0, 1, 0, 1]);
+    expect(diagram.isAnimating()).toBe(false);
+  });
+  test('Velocity different durations', () => {
+    a.animations.new()
+      .scenario({
+        target: {
+          isShown: false,
+          color: [0, 1, 0, 1],
+          position: [1, 1],
+          scale: 2,
+          rotation: 1,
+        },
+        velocity: {
+          position: 0.1,
+          scale: 0.2,
+          rotation: 0.3,
+          color: 0.4,
+          opacity: 0.5,
+        },
+        allSameDuration: false,
+        progression: 'linear',
+      })
+      .start();
+
+    diagram.mock.timeStep(0);
+    expect(a.getPosition()).toEqual(new Point(0, 0));
+    expect(a.getScale()).toEqual(new Point(1, 1));
+    expect(a.getRotation()).toBe(0);
+    expect(a.opacity).toBe(1);
+    expect(a.isShown).toBe(true);
+    expect(a.color).toEqual([1, 0, 0, 1]);
+    diagram.mock.timeStep(1);
+    expect(a.getPosition()).toEqual(new Point(0.1, 0.1));
+    expect(a.getScale()).toEqual(new Point(1.2, 1.2));
+    expect(a.getRotation()).toBe(0.3);
+    expect(a.opacity).toBe(0.5005);
+    expect(a.isShown).toBe(true);
+    expect(a.color).toEqual([0.6, 0.4, 0, 1]);
+    diagram.mock.timeStep(10);
+    expect(a.getPosition()).toEqual(new Point(1, 1));
+    expect(a.getScale()).toEqual(new Point(2, 2));
+    expect(a.getRotation()).toBe(1);
+    expect(a.opacity).toBe(1);
+    expect(a.isShown).toBe(false);
+    expect(a.color).toEqual([0, 1, 0, 1]);
+    expect(diagram.isAnimating()).toBe(false);
+  });
   test('Velocity zero threshold', () => {});
   test('Velocity maxTime', () => {});
   test('Velocity 0 movement', () => {});
