ee6c3ba032a1862b5c3b6e976ea135261531da3c
“airladon” <airladon@gmail.com>
2020-07-10 06:41:12 -0700

    update moving freely test with durations

--- src/js/diagram/Element.moveFreely.test.js
+++ src/js/diagram/Element.moveFreely.test.js
@@ -31,6 +31,7 @@ describe('Move Freely', () => {
       },
     ]);
     a = diagram.elements._a;
+    a.move.freely.deceleration.translation = 1;
     a.setMovable(true);
     click = jest.fn();
     a.onClick = click;
@@ -77,8 +78,34 @@ describe('Move Freely', () => {
       diagram.mock.touchMove([1, 1]);
       expect(a.getPosition().round(3)).toEqual(new Point(1, 1));
     });
-    test('Release', () => {
-      
+    test('Moving Freely', () => {
+      diagram.mock.timeStep(0);
+      diagram.mock.touchDown([0, 0]);
+      diagram.mock.touchMove([-5, 0]);
+      diagram.mock.timeStep(1);
+      diagram.mock.touchMove([0, 0]);
+      expect(a.getPosition().round(3)).toEqual(new Point(0, 0));
+      diagram.mock.touchUp();
+      expect(a.getPosition().round(3)).toEqual(new Point(0, 0));
+      diagram.mock.timeStep(0);
+      expect(a.getRemainingMovingFreelyDuration()).toBe(5);
+      diagram.mock.timeStep(1);
+      expect(a.getPosition().round(3)).toEqual(new Point(4.5, 0));
+      expect(a.getRemainingMovingFreelyDuration()).toBe(4);
+      diagram.mock.timeStep(1);
+      expect(a.getPosition().round(3)).toEqual(new Point(8, 0));
+      expect(a.getRemainingMovingFreelyDuration()).toBe(3);
+      diagram.mock.timeStep(1);
+      expect(a.getPosition().round(3)).toEqual(new Point(10.5, 0));
+      expect(a.getRemainingMovingFreelyDuration()).toBe(2);
+      diagram.mock.timeStep(1);
+      expect(a.getPosition().round(3)).toEqual(new Point(12, 0));
+      expect(a.getRemainingMovingFreelyDuration()).toBe(1);
+      expect(a.state.isMovingFreely).toBe(true);
+      diagram.mock.timeStep(1);
+      expect(a.getPosition().round(3)).toEqual(new Point(12.5, 0));
+      expect(a.getRemainingMovingFreelyDuration()).toBe(0);
+      expect(a.state.isMovingFreely).toBe(false);
     })
   });
 })
