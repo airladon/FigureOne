cf7f31cd88fd1d5975520ea73beccd5a0babb31e
airladon <airladon@gmail.com>
2020-07-27 14:57:06 -0700

    add middle animation tests

--- src/js/diagram/Diagram.stop.test.js
+++ src/js/diagram/Diagram.stop.test.js
@@ -68,6 +68,22 @@ describe('Animate To State', () => {
         diagram.stop('cancel');
         expect(state()).toEqual([false, 0, 0, 0, 1, false]);
       });
+      test('Cancel with animation determining completion as complete', () => {
+        a.animations.new()
+          .position({ start: [0, 0], target: [1, 1], duration: 2 })
+          .ifCancelled('complete')
+          .start('now');
+        diagram.stop('cancel');
+        expect(state()).toEqual([false, 0, 1, 0, 1, false]);
+      });
+      test('Cancel with animation determining completion as freeze', () => {
+        a.animations.new()
+          .position({ start: [0, 0], target: [1, 1], duration: 2 })
+          .ifCancelled('freeze')
+          .start('now');
+        diagram.stop('cancel');
+        expect(state()).toEqual([false, 0, 0, 0, 1, false]);
+      });
       test('Complete', () => {
         diagram.stop('complete');
         expect(state()).toEqual([false, 0, 1, 0, 1, false]);
@@ -89,6 +105,71 @@ describe('Animate To State', () => {
 
         diagram.mock.timeStep(0.4);
         expect(state()).toEqual([true, 0.2, 0, 1, 0, true]);
+        expect(round(diagram.elements.opacity)).toBe(1);
+
+        diagram.mock.timeStep(0.2);
+        expect(state()).toEqual([true, 0.8, 1, 1, 0, true]);
+        expect(round(diagram.elements.opacity)).toBe(0.001);
+
+        diagram.mock.timeStep(0.4);
+        expect(state()).toEqual([true, 0.4, 1, 1, 0, true]);
+        expect(round(diagram.elements.opacity)).toBe(0.5005);
+
+        diagram.mock.timeStep(0.4);
+        expect(state()).toEqual([false, 0, 1, 1, 1, false]);
+      });
+    });
+    describe('Middle', () => {
+      beforeEach(() => {
+        expect(state()).toEqual([true, 2, 0, 0, 0, false]);
+        diagram.mock.timeStep(1);
+        expect(state()).toEqual([true, 1, 0.5, 0, 0, false]);
+      });
+      test('Freeze', () => {
+        diagram.stop('freeze');
+        expect(state()).toEqual([false, 0, 0.5, 0, 1, false]);
+      });
+      test('Cancel', () => {
+        diagram.stop('cancel');
+        expect(state()).toEqual([false, 0, 0.5, 0, 1, false]);
+      });
+      test('Cancel with animation determining completion as complete', () => {
+        a.animations.new()
+          .position({ start: [0, 0], target: [1, 1], duration: 2 })
+          .ifCancelled('complete')
+          .start('now');
+        diagram.mock.timeStep(1);
+        diagram.stop('cancel');
+        expect(state()).toEqual([false, 0, 1, 0, 1, false]);
+      });
+      test('Cancel with animation determining completion as freeze', () => {
+        a.animations.new()
+          .position({ start: [0, 0], target: [1, 1], duration: 2 })
+          .ifCancelled('freeze')
+          .start('now');
+        diagram.mock.timeStep(1);
+        diagram.stop('cancel');
+        expect(state()).toEqual([false, 0, 0.5, 0, 1, false]);
+      });
+      test('Complete', () => {
+        diagram.stop('complete');
+        expect(state()).toEqual([false, 0, 1, 0, 1, false]);
+      });
+      test('Animate To Complete', () => {
+        diagram.stop('animateToComplete');
+        expect(state()).toEqual([true, 1, 0.5, 1, 0, true]);
+        diagram.mock.timeStep(1);
+        expect(state()).toEqual([false, 0, 1, 1, 1, false]);
+      });
+      test('Dissolve To Complete', () => {
+        diagram.stop('dissolveToComplete');
+        expect(state()).toEqual([true, 1, 0.5, 1, 0, true]);
+        diagram.mock.timeStep(0.4);
+        expect(state()).toEqual([true, 0.6, 0.5, 1, 0, true]);
+        expect(round(diagram.elements.opacity)).toBe(0.5005);
+
+        diagram.mock.timeStep(0.4);
+        expect(state()).toEqual([true, 0.2, 0.5, 1, 0, true]);
         expect(round(diagram.elements.opacity)).toBe(1);
 
         diagram.mock.timeStep(0.2);
