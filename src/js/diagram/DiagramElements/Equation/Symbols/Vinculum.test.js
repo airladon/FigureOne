ab9e1552f23e07aa315328bbbf8139ac5139a574
airladon <airladon@gmail.com>
2020-10-28 21:40:21 -0700

    Add tests for border fix

--- src/js/diagram/DiagramElements/Equation/Symbols/Vinculum.test.js
+++ src/js/diagram/DiagramElements/Equation/Symbols/Vinculum.test.js
@@ -1,6 +1,6 @@
-// import {
-//   Point,
-// } from '../../../../tools/g2';
+import {
+  Rect,
+} from '../../../../tools/g2';
 import {
   round,
 } from '../../../../tools/math';
@@ -40,4 +40,17 @@ describe('Equation Symbols - Vinculum', () => {
     diagram.setFirstTransform();
     expect(round(eqn._v.drawingObject.points)).toMatchSnapshot();
   });
+  test('Border calculated using getTransform and not this.transform', () => {
+    eqn.showForm('0');
+    diagram.initialize();
+    const a = eqn._a.getBoundingRect('local').round(3);
+    const b = eqn._b.getBoundingRect('local').round(3);
+    const v = eqn._v.getBoundingRect('local').round(3);
+    const e = eqn.getBoundingRect('local').round(3);
+
+    expect(a).toEqual(new Rect(0.035, 0.094, 0.07, 0.072).round(3));
+    expect(b).toEqual(new Rect(0.035, -0.09, 0.07, 0.104).round(3));
+    expect(v).toEqual(new Rect(0, 0.049, 0.14, 0.01).round(3));
+    expect(e).toEqual(new Rect(0, -0.09, 0.14, 0.256).round(3));
+  })
 });
