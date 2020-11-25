9a3c3af5d7a6aaa1bb938e3ee14d1882e84d1ac3
airladon <airladon@gmail.com>
2020-11-25 12:08:17 -0800

    working on example

--- examples/tester/index.js
+++ examples/tester/index.js
@@ -1,4 +1,4 @@
-const figure = new Fig.Figure({ limits: [-2, -1, 4, 2], color: [1, 0, 0, 1], lineWidth: 0.01, font: { size: 0.1 } });
+const figure = new Fig.Figure({ limits: [-2, -1.5, 4, 3], color: [1, 0, 0, 1], lineWidth: 0.01, font: { size: 0.1 } });
 
 // const figure = new Fig.Figure({ limits: [-8, -8, 16, 16], color: [1, 0, 0, 1]});
 // figure.add([
@@ -36,25 +36,160 @@ const figure = new Fig.Figure({ limits: [-2, -1, 4, 2], color: [1, 0, 0, 1], lin
 //   },
 // ]);
 
+
+
+class Queue {
+  // data: Array<number>;
+  // maxLen: number;
+
+  constructor(initialArray) {
+    this.data = initialArray;
+    this.maxLen = this.data.length;
+  }
+
+  add(element, count = 1) {
+    if (count === 1) {
+      this.data.pop();
+      this.data.unshift(element);
+    } else if (count < this.maxLen) {
+      const newElements = [];
+      // Interpolate the points so we don't have steps in the output
+      const delta = (this.data[0] - element) / (count);
+      for (let i = 0; i < count; i += 1) {
+        newElements.push(element + delta * i);
+      }
+      this.data = [...newElements, ...this.data.slice(0, this.data.length - count)];
+    } else {
+      this.data = Array(this.maxLen).fill(element);
+    }
+  }
+}
+
+const r = 0.8;
+const timeDuration = 10;
+const timeStep = 0.02;
+const time = Fig.range(0, timeDuration, timeStep);
+const points = time.map(t => new Fig.Point(t / 10 + r + 0.2, 0));
+
 figure.add([
   {
-    name: 'x',
-    method: 'line',
+    name: 'diagram',
+    method: 'collection',
     options: {
-      length: 3,
-      position: [-1.5, 0],
-      width: 0.005,
-      color: [0.7, 0.7, 0.7, 1],
+      position: [-1, 0],
     },
-  },
-  {
-    name: 'y',
-    method: 'line',
-    options: {
-      length: 3,
-      position: [-1.5, 0],
-      width: 0.005,
-      color: [0.7, 0.7, 0.7, 1],
-    },
+    elements: [
+      {
+        name: 'x',
+        method: 'line',
+        options: {
+          length: 3.6,
+          position: [-r, 0],
+          width: 0.005,
+          color: [0.7, 0.7, 0.7, 1],
+          // dash: [0.01, 0.01],
+          // copy: { along: 'y', step: 0.4, num: 4 },
+        }
+      },
+      {
+        name: 'y',
+        method: 'line',
+        options: {
+          length: r * 2,
+          position: [0, -r],
+          width: 0.005,
+          angle: Math.PI / 2,
+          color: [0.7, 0.7, 0.7, 1],
+          // dash: [0.01, 0.01],
+          // copy: { along: 'x', step: 0.4, num: 4 },
+        },
+      },
+      {
+        name: 'sine',
+        method: 'collections.line',
+        options: {
+          maxLength: 3,
+          width: 0.005,
+          dash: [0.007, 0.02],
+        },
+      },
+      {
+        name: 'rotator',
+        method: 'line',
+        options: {
+          length: r,
+          width: 0.015,
+          touchBorder: 0.5,
+        },
+        mods: {
+          isMovable: true,
+          move: { type: 'rotation' },
+        },
+      },
+      {
+        name: 'line',
+        method: 'polyline',
+        options: {
+          points,
+          width: 0.01,
+        },
+      },
+    ],
   },
 ]);
+
+
+const rotator = figure.getElement('diagram.rotator');
+const sine = figure.getElement('diagram.sine');
+const line = figure.getElement('diagram.line');
+
+const signal = new Queue(Array(time.length).fill(0));
+let stationaryTime = 0;
+let lastTime = new Date().getTime();
+
+rotator.subscriptions.add('setTransform', () => {
+  const angle = rotator.getRotation();
+  const endPoint = Fig.polarToRect(r, angle);
+  sine.setEndPoints(endPoint, [r + 0.2, endPoint.y]);
+  const slowDown = 4;
+  if (stationaryTime < timeDuration) {
+    const currentTime = new Date().getTime();
+    const delta = Math.min(currentTime - lastTime, timeDuration * 1000);
+    const numSteps = Fig.round(delta / 1000 / timeStep, 0);
+    lastTime = currentTime;
+    if (numSteps > 0) {
+      signal.add(endPoint.y, numSteps);
+      const newPoints = signal.data.map(
+        (y, index) => new Fig.Point(time[index] / slowDown + r + 0.2, y),
+      );
+      line.custom.updatePoints({ points: newPoints });
+    }
+    stationaryTime += delta / 1000;
+    figure.animateNextFrame();
+  }
+});
+
+rotator.setRotation(Math.PI / 4);
+
+
+// function updateSine() {
+//   // if (this.custom.recordState === 'pause') {
+//   //   return;
+//   // }
+//   const slowDown = 4;
+//   if (stationaryTime < timeDuration) {
+//     const currentTime = new Date().getTime();
+//     const delta = Math.min(currentTime - lastTime, timeDuration * 1000);
+//     const numSteps = round(delta / 1000 / timeStep, 0);
+//     lastTime = currentTime;
+//     if (numSteps > 0) {
+//       signal.add(this._rotator._v.p2.y, numSteps);
+//       const newPoints = this.signal.data.map(
+//         (y, index) => new Point(this.layout.time[index] / slowDown, y),
+//       );
+//       this._rotator._sine.updatePoints(newPoints);
+//     }
+//     stationaryTime += delta / 1000;
+//     this.diagram.animateNextFrame();
+//   }
+// }
