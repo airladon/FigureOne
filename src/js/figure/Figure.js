a83ad9de70cb56777b5af117332b278f8bdd8f82
airladon <airladon@gmail.com>
2020-11-25 10:56:21 -0800

    update version

--- src/js/figure/Figure.js
+++ src/js/figure/Figure.js
@@ -167,7 +167,7 @@ export type OBJ_Figure = {
  * <body>
  *     <div id="figureOneContainer" style="width: 800px; height: 800px; background-color: white;">
  *     </div>
- *     <script type="text/javascript" src='https://cdn.jsdelivr.net/npm figureone@0.3.7/figureone.min.js'></script>
+ *     <script type="text/javascript" src='https://cdn.jsdelivr.net/npm figureone@0.3.8/figureone.min.js'></script>
  *     <script type="text/javascript" src='./index.js'></script>
  * </body>
  * </html>
