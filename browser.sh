256f8b128d9f905f9cb392c9945fb6f3359fa9eb
airladon <airladon@gmail.com>
2020-12-07 12:52:05 -0800

    better dockerfile

--- browser.sh
+++ browser.sh
@@ -1,6 +1,10 @@
 PROJECT_PATH=`pwd`
 
+cp containers/playwright/Dockerfile Dockerfile
+docker build -t playwright .
+rm Dockerfile
+
 docker run -it --rm --ipc=host \
   -v $LOCAL_PROJECT_PATH/src:/src \
   -v $LOCAL_PROJECT_PATH/package:/package \
-  mcr.microsoft.com/playwright:bionic /bin/bash
+  playwright /bin/bash
--- start_env.sh
+++ start_env.sh
@@ -24,6 +24,8 @@ docker run -it --rm \
   -v $PROJECT_PATH/.stylelintignore:/opt/app/.stylelintignore \
   -v $PROJECT_PATH/.eslintignore:/opt/app/.eslintignore \
   -v $PROJECT_PATH/browser.sh:/opt/app/browser.sh \
+  -v $PROJECT_PATH/containers:/opt/app/containers \
+  -v $PROJECT_PATH/.dockerignore:/opt/app/.dockerignore \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -e LOCAL_PROJECT_PATH=$PROJECT_PATH \
   -p 8080:8080 \
