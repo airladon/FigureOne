b09edc38d12ee9ba6462380d0da4f4976829d746
airladon <airladon@gmail.com>
2020-12-12 19:31:01 -0800

    simplify testAnimation for jest

--- src/tests/collections/line/animations/animations.test.js
+++ src/tests/collections/line/animations/animations.test.js
@@ -1,24 +1,5 @@
-import makeFigure from '../../../../js/__mocks__/makeFigure';
-import { simpleElement } from '../../../tools';
+import { testAnimation } from '../../../tools';
 
 const { getShapes, updates } = require('./animations.js');
 
-const tests = getShapes(() => ({ x: 0, y: 0 })).map(s => [s.name, s]);
-
-describe('Collection: Line', () => {
-  let figure;
-  beforeEach(() => {
-    figure = makeFigure();
-  });
-  test.each(tests)(
-    '%s',
-    (name, shape) => {
-      figure.add(shape);
-      const element = figure.getElement(name);
-      if (updates[name] != null) {
-        updates[name](element);
-      }
-      expect(simpleElement(element)).toMatchSnapshot();
-    },
-  );
-});
+testAnimation('Collection: Line', getShapes, updates);
