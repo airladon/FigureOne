8795b31528152dc7532cea3f6be612d6620aa7e2
airladon <airladon@gmail.com>
2020-12-14 21:41:38 -0800

    update collection test

--- src/tests/collections/collection/shapes.js
+++ src/tests/collections/collection/shapes.js
@@ -1,12 +1,53 @@
-let tools;
-if (typeof process === 'object') {
-  // eslint-disable-next-line global-require
-  tools = require('../../../index.js').default.tools;
-} else {
-  // eslint-disable-next-line no-undef
-  tools = Fig.tools;
-}
+var { tools } = Fig;
+const { Figure } = Fig;
+const canvas = document.getElementById('figureOneContainer');
+const width = canvas.offsetWidth;
+const height = canvas.offsetHeight;
+const pixelsPerUnit = 200;
+const figureWidth = width / pixelsPerUnit;
+const figureHeight = height / pixelsPerUnit;
 
+const figure = new Figure({
+  limits: [-figureWidth / 2, -figureHeight / 2, figureWidth, figureHeight],
+  color: [1, 0, 0, 1],
+  lineWidth: 0.01,
+  font: { size: 0.1 },
+});
+
+figure.add([
+  {
+    name: 'origin',
+    method: 'polygon',
+    options: {
+      radius: 0.01,
+      line: { width: 0.01 },
+      sides: 10,
+      color: [0.7, 0.7, 0.7, 1],
+    },
+  },
+  {
+    name: 'grid',
+    method: 'grid',
+    options: {
+      bounds: [-figureWidth / 2, -figureHeight / 2, figureWidth, figureHeight],
+      yStep: 0.1,
+      xStep: 0.1,
+      color: [0.9, 0.9, 0.9, 1],
+      line: { width: 0.004 },
+    },
+  },
+  {
+    name: 'gridMajor',
+    method: 'grid',
+    options: {
+      bounds: [-figureWidth / 2, -figureHeight / 2, figureWidth, figureHeight],
+      yStep: 0.5,
+      xStep: 0.5,
+      color: [0.7, 0.7, 0.7, 1],
+      line: { width: 0.004 },
+    },
+  },
+]);
 
 // ***************************************************
 // ***************************************************
