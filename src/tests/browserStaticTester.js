997410633fabdf952704c6141acd452665c144aa
airladon <airladon@gmail.com>
2020-12-21 11:16:21 -0800

    static save in heights of 1000

--- src/tests/browserStaticTester.js
+++ src/tests/browserStaticTester.js
@@ -1,5 +1,5 @@
 /* global page */
-/* eslint-disable jest/no-export */
+/* eslint-disable jest/no-export, no-await-in-loop */
 // eslint-disable-next-line import/no-unresolved
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
@@ -19,8 +19,30 @@ function browserStaticTester(title, file) {
   jest.setTimeout(60000);
   test(title, async () => {
     await page.goto(file);
-    const image = await page.screenshot({ fullPage: true });
-    expect(image).toMatchImageSnapshot();
+    const dimensions = await page.evaluate(() => ({
+      width: document.documentElement.scrollWidth,
+      height: document.documentElement.scrollHeight,
+    }));
+    await page.setViewportSize({ width: dimensions.width, height: 1000 });
+    let remainingHeight = dimensions.height;
+    let y = 0;
+    while (remainingHeight > 0) {
+      await page.evaluate((scrollY) => {
+        window.scroll(0, scrollY);
+      }, [y]);
+      const image = await page.screenshot({
+        // fullPage: true,
+        clip: {
+          x: 0,
+          y: 0,
+          width: dimensions.width,
+          height: Math.min(remainingHeight, 1000),
+        },
+      });
+      y += 1000;
+      remainingHeight -= 1000;
+      expect(image).toMatchImageSnapshot();
+    }
     expect(failures).toEqual([]);
   });
 }
