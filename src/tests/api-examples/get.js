d398871b504d65794e00667627e3394852b9a99f
airladon <airladon@gmail.com>
2020-12-22 16:25:45 -0800

    test tool build ongling

--- src/tests/api-examples/get.js
+++ src/tests/api-examples/get.js
@@ -1,7 +1,7 @@
 
 const Path = require('path');
 const fs = require('fs');
-const readline = require('readline');
+const crypto = require('crypto');
 
 const files = [];
 
@@ -22,40 +22,97 @@ function ThroughDirectory(directory) {
 
 ThroughDirectory(`${__dirname}/../../js`);
 
-const examples = [];
+const examples = {};
 for (let i = 0; i < files.length; i += 1) {
+  let lastInterfaceName = ''
   const file = files[i];
   const data = fs.readFileSync(file, 'UTF-8');
   const lines = data.split(/\r?\n/);
-  const uniqueFileId = file.match(/\/src\/.*/)[0].replace(/\//g, '.');
-  console.log(uniqueFileId);
-  let exampleCount = 0;
+  const uniqueFileId = file.match(/\/src\/.*/)[0].replace(/\/src\/js\//, '').replace(/\//g, '.');
+  // console.log(uniqueFileId);
+  // let exampleCount = 0;
 
   // print all lines
   let inExample = false;
   let currentExample = [];
+  let unnamedExamples = {};
+  let interfaceName = '';
+  let readInterfaceName = false;
+  const addExample = () => {
+    const code = currentExample.join('\n');
+    const hash = crypto.createHash('sha1').update(code).digest('hex');
+    unnamedExamples[hash] = code;
+    currentExample = [];
+    // inExample = false;
+  };
   lines.forEach((line) => {
+    if (line.match(/^ *\/\*/)) {
+      interfaceName = '';
+    }
     if (line.match(/^ *\* *@example/)) {
-      exampleCount += 1;
+      // exampleCount += 1;
       // console.log(file, line)
       if (inExample) {
-        examples.push(currentExample.join('\n'));
-        currentExample = [];
+        // examples.push(currentExample.join('\n'));
+        // currentExample = [];
+        addExample();
       }
       inExample = true;
       return;
     }
     if (inExample && line.match(/^ *\*\//)) {
       inExample = false;
-      examples.push(currentExample.join('\n'));
-      currentExample = [];
+      // examples.push(currentExample.join('\n'));
+      // currentExample = [];
+      addExample();
+      readInterfaceName = true;
       return;
     }
     if (inExample) {
       currentExample.push(line.slice().replace(/^ *\*/, ''));
     }
+    if (readInterfaceName && !line.match(/^ *\/\//) && !line.match(/^ *\/\*/)) {
+      let updateName = true;
+      let nameFound = true;
+      if (line.match(/^ *export type/)) {
+        [, interfaceName] = line.match(/export type ([^ ]*)/);
+      } else if (line.match(/^ *class/)) {
+        [, interfaceName] = line.match(/class ([^ ]*)/);
+      } else if (line.match(/function/)) {
+        [, interfaceName] = line.match(/function ([^(]*)/);
+      } else if (line.match(/^ *export default class/)) {
+        [, interfaceName] = line.match(/class ([^ ]*)/);
+      } else if (line.match(/^ *export class/)) {
+        [, interfaceName] = line.match(/class ([^ ]*)/);
+      } else if (line.match(/^ *type/)) {
+        [, interfaceName] = line.match(/type ([^ ]*)/);
+      } else if (line != null && line != '') {
+        [, interfaceName] = line.match(/^ *([^(]*)/);
+        updateName = false;
+      } else {
+        nameFound = false;
+      }
+      if (!nameFound) {
+        return;
+      }
+      if (updateName) {
+        lastInterfaceName = interfaceName;
+      } else {
+        interfaceName = `${lastInterfaceName}.${interfaceName}`;
+      }
+      readInterfaceName = false;
+      // console.log(interfaceName, '::', crypto.createHash('sha1').update(interfaceName).digest('hex'))
+      Object.keys(unnamedExamples).forEach((hash) => {
+        const id = `${uniqueFileId}.${interfaceName}.${hash}`;
+        // console.log(id)
+        examples[id] = unnamedExamples[hash];
+      });
+      unnamedExamples = {};
+    }
   });
 
+  console.log(Object.keys(examples))
+  
   // let inExample = false;
   // let currentExample = [];
   // rl.on('line', (line) => {
@@ -80,5 +137,5 @@ for (let i = 0; i < files.length; i += 1) {
   // });
 }
 
-console.log(examples.slice(0,3));
+// console.log(examples.slice(0,3));
 
