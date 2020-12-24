19fbca954aabefe6d740df0bb19e8d99a336aef9
airladon <airladon@gmail.com>
2020-12-23 20:33:50 -0800

    update tester with wave eqn test

--- tester/index.js
+++ tester/index.js
@@ -38,200 +38,177 @@ const figure = new Fig.Figure({
 //   },
 // ]);
 
-// const { Transform } = Fig;
-// const { range } = Fig.tools.math;
+const { Transform } = Fig;
+const { range } = Fig.tools.math;
 
-// class Recording {
-//   constructor(initialValue) {
-//     // This data class will hold signal data for the most recent 10s at a
-//     // resolution (sampling rade) of 0.02s.
-//     this.duration = 10;
-//     this.timeStep = 0.01;
-//     this.len = this.duration / this.timeStep;
-//     this.data = Array(this.len).fill(initialValue);
-//     // this.data[0] = initialValue;
-//     // console.log(this.data)
+class Recording {
+  constructor(initialValue) {
+    // This data class will hold signal data for the most recent 10s at a
+    // resolution (sampling rade) of 0.02s.
+    this.duration = 10;
+    this.timeStep = 0.01;
+    this.len = this.duration / this.timeStep;
+    this.data = Array(this.len).fill(initialValue);
+    // this.data[0] = initialValue;
+    // console.log(this.data)
 
-//     // record the current time
-//     this.lastTime = new Date().getTime();
-//     console.log(this.len)
-//   }
+    // record the current time
+    this.lastTime = new Date().getTime();
+    console.log(this.len)
+  }
 
-//   // Update the signal data with the new value. Signal data is has a resolution
-//   // of 0.02s, so if this value comes in more than 0.04s after the last value
-//   // was recorder, then use interpolation to fill in the missing samples.
-//   update(value) {
-//     const currentTime = new Date().getTime();
-//     const deltaTime = (currentTime - this.lastTime) / 1000;
+  // Update the signal data with the new value. Signal data is has a resolution
+  // of 0.02s, so if this value comes in more than 0.04s after the last value
+  // was recorder, then use interpolation to fill in the missing samples.
+  update(value) {
+    const currentTime = new Date().getTime();
+    const deltaTime = (currentTime - this.lastTime) / 1000;
 
-//     // If the value has come in faster than the time resolution, then
-//     // do nothing
-//     if (deltaTime < this.timeStep) {
-//       return;
-//     }
+    // If the value has come in faster than the time resolution, then
+    // do nothing
+    if (deltaTime < this.timeStep) {
+      return;
+    }
 
-//     this.lastTime = currentTime;
+    this.lastTime = currentTime;
 
-//     // Count the number of samples that need to be added to the signal
-//     const count = Math.floor(deltaTime / this.timeStep);
+    // Count the number of samples that need to be added to the signal
+    const count = Math.floor(deltaTime / this.timeStep);
 
-//     // Interpolate between the last recorded value and the new value
-//     const newValues = [];
-//     const deltaValue = (this.data[0] - value) / (count);
-//     for (let i = 0; i < count; i += 1) {
-//       newValues.push(value + deltaValue * i);
-//     }
-//     this.data = [...newValues, ...this.data.slice(0, this.len - count)];
-//   }
+    // Interpolate between the last recorded value and the new value
+    const newValues = [];
+    const deltaValue = (this.data[0] - value) / (count);
+    for (let i = 0; i < count; i += 1) {
+      newValues.push(value + deltaValue * i);
+    }
+    this.data = [...newValues, ...this.data.slice(0, this.len - count)];
+  }
 
-//   getY(timeDelta) {
-//     const index = Math.floor(timeDelta / this.timeStep + this.timeStep / 10);
-//     // console.log(index)
-//     return this.data[index];
-//   }
+  getY(timeDelta) {
+    const index = Math.floor(timeDelta / this.timeStep + this.timeStep / 10);
+    // console.log(index)
+    return this.data[index];
+  }
 
-//   // // Make an array of points where this.data is plotted against this.x
-//   // getPoints() {
-//   //   return this.data.map((value, index) => new Fig.Point(this.x[index], value));
-//   // }
-// }
+  // // Make an array of points where this.data is plotted against this.x
+  // getPoints() {
+  //   return this.data.map((value, index) => new Fig.Point(this.x[index], value));
+  // }
+}
 
-// const startX = -1.5;
+const startX = -1.5;
 
-// figure.add([
-//   {
-//     name: 'xAxis',
-//     method: 'collections.axis',
-//     options: {
-//       start: 0,
-//       stop: 3.2,
-//       length: 3.2,
-//       line: { width: 0.005, arrow: { end: 'barb' } },
-//       ticks: { step: 0.5, length: 0.1 },
-//       labels: { font: { size: 0.08 }, text: ['0'] },
-//       position: [-1.5, 0],
-//       title: {
-//         font: { style: 'italic', family: 'serif', size: 0.12 },
-//         text: ['x', { font: { size: 0.06 }, lineSpace: 0.08, text: 'meters' }],
-//         position: [3.3, 0.03],
-//       },
-//     },
-//   },
-//   {
-//     name: 'yAxis',
-//     method: 'collections.axis',
-//     options: {
-//       axis: 'y',
-//       start: -1.1,
-//       stop: 1.1,
-//       length: 2.2,
-//       line: { width: 0.005, arrow: 'barb' },
-//       // ticks: { step: 0.5, length: 0.1 },
-//       // labels: { font: { size: 0.08 }, text: ['0'] },
-//       position: [-1.5, -1.1],
-//       title: {
-//         font: { style: 'italic', family: 'serif', size: 0.12 },
-//         text: ['y', { font: { size: 0.06 }, lineSpace: 0.08, text: 'meters' }],
-//         // position: [1.2, 0],
-//         rotation: 0,
-//         offset: [0.1, 1.2],
-//       },
-//     },
-//   },
-// ]);
-
-
-// const ball = (x, index) => ({
-//   name: `ball${index}`,
-//   method: 'primitives.polygon',
-//   options: {
-//     sides: 10,
-//     radius: 0.02,
-//     transform: new Transform().translate(x, 0),
-//   },
-//   mods: {
-//     dimColor: [0.5, 0.5, 0.5, 0.8],
-//   },
-// });
-
-
-// const f = 0.3;
-// const A = 1;
-// // const lambda = 2;
-// // const k = 2 * Math.PI / lambda;
-// const c = 1;
-// const yB0 = t => A * Math.sin(2 * Math.PI * f * t);
-
-// const xValues = range(-1.46, 1.5, 0.04);
-// figure.add(ball(-1.5, 0));
-// const data = new Recording(0);
-
-// const b0 = figure.getElement('ball0');
-// b0.setMovable();
-// b0.touchBorder = 0.2;
-// b0.move.bounds = {
-//   translation: { left: startX, right: startX, bottom: -A, top: A }
-// };
-
-// xValues.forEach((x, index) => {
-//   figure.add(ball(x, index + 1));
-//   const b = figure.getElement(`ball${index + 1}`);
-//   b.custom.x = x;
-// });
-
-// // Update function for everytime we want to update the signal
-// function update() {
-//   const { y } = figure.elements._ball0.transform.order[0];
-//   data.update(y);
-//   for (let i = 1; i < xValues.length + 1; i += 1) {
-//     const b = figure.elements[`_ball${i}`];
-//     const by = data.getY((b.custom.x - startX) / c);
-//     b.setPosition(b.custom.x, by);
-//   }
-// }
-
-// // Before each draw, update the points
-// figure.subscriptions.add('beforeDraw', () => {
-//   update();
-// });
-
-// // After each draw, call a next animation frame so udpates happen on each frame
-// figure.subscriptions.add('afterDraw', () => {
-//   figure.animateNextFrame();
-// });
-
-// const disturb = () => {
-//   b0.animations.new()
-//     .delay(1)
-//     .position({ duration: 0.3, target: [startX, 0.2], progression: 'easeout' })
-//     .position({ duration: 0.3, target: [startX, 0], progression: 'easein' })
-//     .start();
-// };
-// disturb();
-
-// // figure.getElement('ball10').setColor([1, 0, 0, 1])
-// // figure.getElement('ball0').setColor([1, 0, 0, 1])
-// figure.elements.highlight(['ball0', 'ball10']);
-
-
-figure.add({
-  name: 'eqn',
-  method: 'equation',
-  options: {
-    elements: {
-      times: ' \u00D7 ',
-      equals: ' = ',
-      v: { symbol: 'vinculum' },
-    },
-    forms: {
-      1: ['a', 'equals', 'b', 'times', 'c'],
-      2: ['b', 'equals', { frac: ['a', 'v', 'c'] }],
+figure.add([
+  {
+    name: 'xAxis',
+    method: 'collections.axis',
+    options: {
+      start: 0,
+      stop: 3.2,
+      length: 3.2,
+      line: { width: 0.005, arrow: { end: 'barb' } },
+      ticks: { step: 0.5, length: 0.1 },
+      labels: { font: { size: 0.08 }, text: ['0'] },
+      position: [-1.5, 0],
+      title: {
+        font: { style: 'italic', family: 'serif', size: 0.12 },
+        text: ['x', { font: { size: 0.06 }, lineSpace: 0.08, text: 'meters' }],
+        position: [3.3, 0.03],
+      },
     },
   },
+  {
+    name: 'yAxis',
+    method: 'collections.axis',
+    options: {
+      axis: 'y',
+      start: -1.1,
+      stop: 1.1,
+      length: 2.2,
+      line: { width: 0.005, arrow: 'barb' },
+      // ticks: { step: 0.5, length: 0.1 },
+      // labels: { font: { size: 0.08 }, text: ['0'] },
+      position: [-1.5, -1.1],
+      title: {
+        font: { style: 'italic', family: 'serif', size: 0.12 },
+        text: ['y', { font: { size: 0.06 }, lineSpace: 0.08, text: 'meters' }],
+        // position: [1.2, 0],
+        rotation: 0,
+        offset: [0.1, 1.2],
+      },
+    },
+  },
+]);
+
+
+const ball = (x, index) => ({
+  name: `ball${index}`,
+  method: 'primitives.polygon',
+  options: {
+    sides: 10,
+    radius: 0.02,
+    transform: new Transform().translate(x, 0),
+  },
+  mods: {
+    dimColor: [0.5, 0.5, 0.5, 0.8],
+  },
 });
-figure.elements._eqn.showForm('1');
-figure.elements._eqn.goToForm({
-  form: 2,
-  animate: 'move',
-  delay: 1,
+
+
+const f = 0.3;
+const A = 1;
+// const lambda = 2;
+// const k = 2 * Math.PI / lambda;
+const c = 1;
+const yB0 = t => A * Math.sin(2 * Math.PI * f * t);
+
+const xValues = range(-1.46, 1.5, 0.04);
+figure.add(ball(-1.5, 0));
+const data = new Recording(0);
+
+const b0 = figure.getElement('ball0');
+b0.setMovable();
+b0.touchBorder = 0.2;
+b0.move.bounds = {
+  translation: { left: startX, right: startX, bottom: -A, top: A }
+};
+
+xValues.forEach((x, index) => {
+  figure.add(ball(x, index + 1));
+  const b = figure.getElement(`ball${index + 1}`);
+  b.custom.x = x;
 });
+
+// Update function for everytime we want to update the signal
+function update() {
+  const { y } = figure.elements._ball0.transform.order[0];
+  data.update(y);
+  for (let i = 1; i < xValues.length + 1; i += 1) {
+    const b = figure.elements[`_ball${i}`];
+    const by = data.getY((b.custom.x - startX) / c);
+    b.setPosition(b.custom.x, by);
+  }
+}
+
+// Before each draw, update the points
+figure.subscriptions.add('beforeDraw', () => {
+  update();
+});
+
+// After each draw, call a next animation frame so udpates happen on each frame
+figure.subscriptions.add('afterDraw', () => {
+  figure.animateNextFrame();
+});
+
+const disturb = () => {
+  b0.animations.new()
+    .delay(1)
+    .position({ duration: 0.3, target: [startX, 0.2], progression: 'easeout' })
+    .position({ duration: 0.3, target: [startX, 0], progression: 'easein' })
+    .start();
+};
+disturb();
+
+// figure.getElement('ball10').setColor([1, 0, 0, 1])
+// figure.getElement('ball0').setColor([1, 0, 0, 1])
+figure.elements.highlight(['ball0', 'ball10']);
