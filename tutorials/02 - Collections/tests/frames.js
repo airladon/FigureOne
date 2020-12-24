feac663dd0f428e0d5e7e37370b1c065e4009366
airladon <airladon@gmail.com>
2020-12-23 16:06:20 -0800

    remove collections tutorial change

--- tutorials/02 - Collections/tests/frames.js
+++
@@ -1,8 +0,0 @@
-/* global __duration __frames __timeStep __title */
-/* eslint no-unused-vars: ["error", { "vars": "local" }] */
-/* eslint-disable no-global-assign */
-
-__title = 'Tutorial - Collections';
-__duration = 5;
-__timeStep = 1;
-__frames = [[]];
