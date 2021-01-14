9fd17d0d62e80f518bbfc5e02e0e07d951615edd
airladon <airladon@gmail.com>
2021-01-14 14:08:40 -0800

    fix shape tutorial tests and add collecitons tutorial tests

---
+++ docs/tutorials/02 - Collections/tests/frames.js
@@ -0,0 +1,16 @@
+/* global __duration __frames __timeStep __title */
+/* eslint no-unused-vars: ["error", { "vars": "local" }] */
+/* eslint-disable no-global-assign */
+
+__title = 'Tutorial - Collections';
+__duration = 5;
+__timeStep = 0.5;
+__frames = [
+  [0, `
+    figure.stop();
+    figure.getElement('c').setRotation(0);
+    figure.getElement('c').animations.new()
+      .rotation({ target: Math.PI * 1.999, direction: 1, duration: 5 })
+      .start();
+  `],
+];
