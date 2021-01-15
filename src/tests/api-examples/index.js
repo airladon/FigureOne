56fd511b4054566cc5726a9a328d1ab4d040f5f9
airladon <airladon@gmail.com>
2021-01-14 22:49:26 -0800

    update snapshots

--- src/tests/api-examples/index.js
+++ src/tests/api-examples/index.js
@@ -43,6 +43,11 @@ figure.add([
 ]);
 
 
+const pow = (pow = 2, stop = 10, step = 0.05) => {
+  const xValues = Fig.tools.math.range(0, stop, step);
+  return xValues.map(x => new Fig.Point(x, x ** pow));
+}
+
 
 figure.globalAnimation.setManualFrames();
 figure.globalAnimation.frame(0);
