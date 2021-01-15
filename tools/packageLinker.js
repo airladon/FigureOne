138f6d91b3c1a42cd186d11888c15883d5e54b97
airladon <airladon@gmail.com>
2021-01-14 22:18:48 -0800

    update tools and html files to dev

--- tools/packageLinker.js
+++ tools/packageLinker.js
@@ -3,16 +3,17 @@ const fs = require('fs');
 const { getFiles } = require('./getFiles');
 const pjson = require('../package.json');
 
-const to = 'remote';
+const to = process.argv.slice(2)[0] || 'dev';
 
-const files = getFiles('./docs/', /html$/, /test.html$/);
+const files = getFiles('./docs/tutorials/', /html$/, /test.html$/);
 
 const remote = `https://cdn.jsdelivr.net/npm/figureone@${pjson.version}/figureone.min.js`;
 for (let i = 0; i < files.length; i += 1) {
   const file = files[i];
+  console.log(file);
   const figOne = {
-    dev: Path.relative(file, './package/index.js'),
-    prod: Path.relative(file, './package/figureone.min.js'),
+    dev: Path.relative(Path.dirname(file), './package/index.js'),
+    prod: Path.relative(Path.dirname(file), './package/figureone.min.js'),
     remote,
   };
 
@@ -21,91 +22,10 @@ for (let i = 0; i < files.length; i += 1) {
   for (let l = 0; l < lines.length; l += 1) {
     const line = lines[l];
     if (line.match(/^ *<script.*figureone.min.js/) || line.match(/^ *<script.*package\/index.js/)) {
-      const leadingSpaces = line.match(/[^ ]/).index;
-      const newLine = `${Array(leadingSpaces).join(' ')}<script type="text/javascript" src="${figOne[to]}"></script>\n`;
+      const leadingSpaces = line.match(/[^ ]/).index + 1;
+      const newLine = `${Array(leadingSpaces).join(' ')}<script type="text/javascript" src="${figOne[to]}"></script>`;
       lines[l] = newLine;
     }
+    fs.writeFileSync(file, lines.join('\n'), 'UTF-8');
   }
 }
-//   //   let lastInterfaceName = '';
-//   //   const file = files[i];
-//   //   const data = fs.readFileSync(file, 'UTF-8');
-//   //   const lines = data.split(/\r?\n/);
-//   //   const uniqueFileId = file.match(/\/src\/.*/)[0]
-//   //     .replace(/\/src\/js\//, '').replace(/\//g, '.');
-//   //   let inExample = false;
-//   //   let currentExample = [];
-//   //   let unnamedExamples = {};
-//   //   let interfaceName = '';
-//   //   let readInterfaceName = false;
-//   //   const addExample = (e, c) => {
-//   //     const code = c.join('\n');
-//   //     const hash = crypto.createHash('sha1').update(code).digest('hex').slice(0, 8);
-//   //     e[hash] = code;
-//   //   };
-//   //   lines.forEach((line) => {
-//   //     if (line.match(/^ *\/\*/)) {
-//   //       interfaceName = '';
-//   //     }
-//   //     if (line.match(/^ *\* *@example/)) {
-//   //       if (inExample) {
-//   //         addExample(unnamedExamples, currentExample);
-//   //         currentExample = [];
-//   //       }
-//   //       inExample = true;
-//   //       return;
-//   //     }
-//   //     if (inExample && line.match(/^ *\*\//)) {
-//   //       inExample = false;
-//   //       addExample(unnamedExamples, currentExample);
-//   //       currentExample = [];
-//   //       readInterfaceName = true;
-//   //       return;
-//   //     }
-//   //     if (inExample) {
-//   //       currentExample.push(line.slice().replace(/^ *\*/, ''));
-//   //     }
-//   //     if (readInterfaceName && !line.match(/^ *\/\//) && !line.match(/^ *\/\*/)) {
-//   //       let updateName = true;
-//   //       let nameFound = true;
-//   //       if (line.match(/^ *export type/)) {
-//   //         [, interfaceName] = line.match(/export type ([^ ]*)/);
-//   //       } else if (line.match(/^ *class/)) {
-//   //         [, interfaceName] = line.match(/class ([^ ]*)/);
-//   //       } else if (line.match(/function/)) {
-//   //         [, interfaceName] = line.match(/function ([^(]*)/);
-//   //       } else if (line.match(/^ *export default class/)) {
-//   //         [, interfaceName] = line.match(/class ([^ ]*)/);
-//   //       } else if (line.match(/^ *export class/)) {
-//   //         [, interfaceName] = line.match(/class ([^ ]*)/);
-//   //       } else if (line.match(/^ *type/)) {
-//   //         [, interfaceName] = line.match(/type ([^ ]*)/);
-//   //       } else if (line != null && line !== '') {
-//   //         [, interfaceName] = line.match(/^ *([^(]*)/);
-//   //         updateName = false;
-//   //       } else {
-//   //         nameFound = false;
-//   //       }
-//   //       if (!nameFound) {
-//   //         return;
-//   //       }
-//   //       if (updateName) {
-//   //         lastInterfaceName = interfaceName;
-//   //       } else {
-//   //         interfaceName = `${lastInterfaceName}.${interfaceName}`;
-//   //       }
-//   //       readInterfaceName = false;
-//   //       Object.keys(unnamedExamples).forEach((hash) => {
-//   //         const id = `${uniqueFileId}.${interfaceName}.${hash}`;
-//   //         examples[id] = unnamedExamples[hash];
-//   //       });
-//   //       unnamedExamples = {};
-//   //     }
-//   //   });
-//   // }
-//   // return examples;
-// }
-// getHTMLFiles();
-// // module.exports = {
-// //   getExamples,
-// // };
