31f7a3d3d13414020293021dbdc654968b4155a6
airladon <airladon@gmail.com>
2021-01-19 13:31:05 -0800

    fix updateVersion

--- tools/updateVersion.js
+++ tools/updateVersion.js
@@ -17,12 +17,16 @@ for (let i = 0; i < files.length; i += 1) {
   const file = files[i];
   const data = fs.readFileSync(file, 'UTF-8');
   const lines = data.split(/\r?\n/);
+  let toUpdate = false;
   for (let l = 0; l < lines.length; l += 1) {
     const line = lines[l];
     if (line.match(/figureone@/)) {
-      const newLine = line.replace(/figureone@[^"]*/, `figureone@${pjson.version}`);
+      toUpdate = true;
+      const newLine = line.replace(/figureone@[^/]*/, `figureone@${pjson.version}`);
       lines[l] = newLine;
     }
-    fs.writeFileSync(file, lines.join('\n'), 'UTF-8');
+    if (toUpdate) {
+      fs.writeFileSync(file, lines.join('\n'), 'UTF-8');
+    }
   }
 }
