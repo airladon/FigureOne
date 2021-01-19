104a72a3ae3c16169a0acfbe597700df78a2499a
airladon <airladon@gmail.com>
2021-01-18 19:55:54 -0800

    udpate api test snapshots with longer animations

--- scratch/simple/index.js
+++ scratch/simple/index.js
@@ -5,41 +5,15 @@ const figure = new Fig.Figure({
   font: { size: 0.1 },
 });
 
-// Two lines animating to 1
-figure.add({
-  name: 'eqn',
+const [eqn] = figure.add({
   method: 'equation',
   options: {
-    elements: {
-      equals1: ' = ',
-      equals2: ' = ',
-    },
     forms: {
-      0: {
-        lines: {
-          content: [
-            {
-              content: ['a_1', 'equals1', 'b', '_ + ', 'c'],
-              justify: 'equals1',
-            },
-            {
-              content: ['d', '_ - ', 'e', 'equals2', 'a_2'],
-              justify: 'equals2',
-            },
-            { frac: ['a', 'vinculum', 'b'] },
-          ],
-          space: 0.08,
-          justify: 'element',
-        },
-      },
-      1: ['d', '_ - ', 'e', 'equals1', 'b', '_ + ', 'c'],
+      form1: { frac: ['a', 'v1_vinculum', { frac: ['c', 'v2_vinculum', 'b'] }] },
+      form2: { frac: [['a', 'ef'], 'v1', { frac: ['c', 'v2', 'd'] }] },
     },
   },
 });
-
-figure.getElement('eqn').showForm(0);
-figure.getElement('eqn').animations.new()
-  .goToForm({
-    delay: 1, target: '1', duration: 1, animate: 'move',
-  })
+eqn.animations.new()
+  .goToForm({ delay: 1, target: 'form2', animate: 'move' })
   .start();
