4660482c0b375c30ecd11dd7b4e289b5741dddbb
airladon <airladon@gmail.com>
2021-05-21 07:40:52 -0700

    pic working

--- scratch/simple/index.js
+++ scratch/simple/index.js
@@ -7,197 +7,102 @@ const figure = new Fig.Figure({
 
 const { rand } = Fig.tools.math;
 
-/*
-.##.....##.....######..##.....##....###....########..########.########.
-.##.....##....##....##.##.....##...##.##...##.....##.##.......##.....##
-.##.....##....##.......##.....##..##...##..##.....##.##.......##.....##
-.##.....##.....######..#########.##.....##.##.....##.######...########.
-..##...##...........##.##.....##.#########.##.....##.##.......##...##..
-...##.##......##....##.##.....##.##.....##.##.....##.##.......##....##.
-....###........######..##.....##.##.....##.########..########.##.....##
-*/
-// Each charge position and value is a uniform. There are 20 charges, and so
-// there are 20 uniforms
-const vertexShader = `
-attribute vec2 a_position;
-attribute vec2 a_pos2;
-attribute vec2 a_pos3;
-attribute vec2 a_pos4;
-uniform mat3 u_matrix;
-uniform float u_percent;
-uniform int u_from;
-uniform int u_to;
+const img = new Image();
+img.src = './logo.png';
 
-void main() {
-  vec2 fromPos = a_position;
-  vec2 toPos = a_pos2;
-  vec2 positions[4];
-
-  positions[0] = a_position;
-  positions[1] = a_pos2;
-  positions[2] = a_pos3;
-  positions[3] = a_pos4;
-
-  for (int i = 0; i < 4; i++) {
-    if (u_from == i) {
-      fromPos = positions[i];
-      break;
-    }
-  }
-  for (int i = 0; i < 4; i++) {
-    if (u_to == i) {
-      toPos = positions[i];
-      break;
-    }
-  }
-
-  vec2 newPosition = (toPos - fromPos) * u_percent + fromPos;
-  gl_Position = vec4((u_matrix * vec3(newPosition.xy, 1)).xy, 0, 1);
-}`;
-
-const points1 = [];
-const points2 = [];
-const points3 = [];
-const points4 = [];
-const colors1 = [];
-const colors2 = [];
-const colors3 = [];
-const colors4 = [];
-const r = 0.02;
-const makeShape = center => [
-  center[0] - r / 2, center[1] - r / 2,
-  center[0] + r / 2, center[1] - r / 2,
-  center[0] + r / 2, center[1] + r / 2,
-  center[0] - r / 2, center[1] - r / 2,
-  center[0] + r / 2, center[1] + r / 2,
-  center[0] - r / 2, center[1] + r / 2,
-];
-const makeColors = c => [...c, ...c, ...c, ...c, ...c, ...c];
-const n = 10000;
-const s = 1;
-for (let i = 0; i < n; i += 1) {
-  const x = -Math.PI * 0.9 + Math.PI * 2 * 0.9 / n * i;
-  // const center1 = [rand(-2, 2), rand(-2, 2)];
-  // points1.push(...makeShape(center1));
-  // const center1 = [rand(-2, 2), 0];
-  const center1 = [x, 0];
-  points1.push(...makeShape(center1));
-  const center2 = [
-    0.9 * Math.cos(Math.PI * 2 / n * i + Math.PI / 4),
-    0.9 * Math.sin(Math.PI * 2 / n * i + Math.PI / 4),
+img.addEventListener('load', () => {
+  // console.log(img.data);
+  const canvas = document.createElement('canvas');
+  const res = 256;
+  canvas.width = res;
+  canvas.height = res;
+  const context = canvas.getContext('2d');
+  context.drawImage(img, 0, 0);
+  // const imgData = context.getImageData(0, 0, res, res).data;
+  // console.log(imgData)
+  const points1 = [];
+  const points2 = [];
+  const points3 = [];
+  const points4 = [];
+  const colors1 = [];
+  const colors2 = [];
+  const colors3 = [];
+  const colors4 = [];
+  const r = 0.02;
+  const makeShape = center => [
+    center[0] - r / 2, center[1] - r / 2,
+    center[0] + r / 2, center[1] - r / 2,
+    center[0] + r / 2, center[1] + r / 2,
+    center[0] - r / 2, center[1] - r / 2,
+    center[0] + r / 2, center[1] + r / 2,
+    center[0] - r / 2, center[1] + r / 2,
   ];
-  points2.push(...makeShape(center2));
-  let center3;
-  if (i < n / 4) {
-    center3 = [s / 2 - s / (n / 4) * i, s / 2];
-  } else if (i < n / 2) {
-    center3 = [-s / 2, s / 2 - s / (n / 4) * (i % (n / 4))];
-  } else if (i < n / 4 * 3) {
-    center3 = [-s / 2 + s / (n / 4) * (i % (n / 4)), -s / 2];
-  } else {
-    center3 = [s / 2, -s / 2 + s / (n / 4) * (i % (n / 4))];
+  const makeColors = c => [...c, ...c, ...c, ...c, ...c, ...c];
+  const n = res * res;
+  const s = 1;
+  for (let i = 0; i < n; i += 1) {
+    const x = -Math.PI * 0.9 + Math.PI * 2 * 0.9 / n * i;
+    const center1 = [rand(-2, 2), 0];
+    points1.push(...makeShape(center1));
+    const center2 = [
+      0.9 * Math.cos(Math.PI * 2 / n * i + Math.PI / 4),
+      0.9 * Math.sin(Math.PI * 2 / n * i + Math.PI / 4),
+    ];
+    points2.push(...makeShape(center2));
+    let center3;
+    if (i < n / 4) {
+      center3 = [s / 2 - s / (n / 4) * i, s / 2];
+    } else if (i < n / 2) {
+      center3 = [-s / 2, s / 2 - s / (n / 4) * (i % (n / 4))];
+    } else if (i < n / 4 * 3) {
+      center3 = [-s / 2 + s / (n / 4) * (i % (n / 4)), -s / 2];
+    } else {
+      center3 = [s / 2, -s / 2 + s / (n / 4) * (i % (n / 4))];
+    }
+    points3.push(...makeShape(center3));
+    const p4 = [-2.5 + (i % res) / res * 5, 2.5 - (Math.floor(i / res) / res * 5)];
+    // console.log(p4)
+    // points4.push(...makeShape([p4.x, p4.y]));
+    points4.push(...makeShape(p4));
+    colors1.push(...makeColors([1, 0, 0, 1]));
+    colors2.push(...makeColors([0, 1, 0, 1]));
+    // colors4.push(...makeColors([1, 1, 0, 1]));
+    // colors3.push(...makeColors([imgData[i] / 255, imgData[n][1] / 255, imgData[n][2] / 255, 1]));
+    colors3.push(...makeColors([1, 1, 0, 1]));
+    // colors4.push(...makeColors([0, 0, 1, 1]));
+
+    // colors4.push(...makeColors([imgData[i * 4] / 255, imgData[i * 4 + 1] / 255, imgData[i * 4 + 2] / 255, 1]))
+    let pixel = context.getImageData(i % res, Math.floor(i / res), 1, 1).data;
+    if (pixel[2] < 100) {
+      pixel = [0, 0, 0, 0];
+    }
+    colors4.push(...makeColors(pixel.map(p => p / 255)))
   }
-  points3.push(...makeShape(center3));
-  // points4.push(...makeShape([x, Math.sin(Math.PI * 2 / 4 * x)]));
-  const p4 = Fig.tools.g2.polarToRect(rand(-3, 3), rand(-3, 3));
-  points4.push(...makeShape([p4.x, p4.y]));
-  colors1.push(...makeColors([1, 0, 0, 1]));
-  colors2.push(...makeColors([0, 1, 0, 1]));
-  colors3.push(...makeColors([0, 0, 1, 1]));
-  colors4.push(...makeColors([0, 1, 1, 1]));
-}
+  console.log(context.canvas.width, context.canvas.height)
+  // console.log(colors1)
+  // console.log(colors4)
+  // console.log(imgData)
 
-// The `field` FigureElement has the arrow grid within it.
-// The vertex shader will orient and scale the arrows based on the
-// superposition of charge contributions from each charge at the vertex the
-// shader is operating on.
-// const trace = figure.add({
-//   make: 'gl',
-//   vertexShader: {
-//     src: vertexShader,
-//     vars: [
-//       'a_position',
-//       'a_pos2',
-//       'a_pos3',
-//       'a_pos4',
-//       'u_matrix',
-//       'u_percent',
-//       'u_from',
-//       'u_to',
-//     ],
-//   },
-//   fragShader: 'simple',
-//   vertices: { data: points1 },
-//   buffers: [
-//     { name: 'a_pos2', data: points2 },
-//     { name: 'a_pos3', data: points3 },
-//     { name: 'a_pos4', data: points4 },
-//   ],
-//   uniforms: [
-//     { name: 'u_percent', length: 1 },
-//     { name: 'u_from', length: 1, type: 'INT' },
-//     { name: 'u_to', length: 1, type: 'INT' },
-//   ],
-//   color: [1, 0, 0, 1],
-// });
+  const m = figure.add({
+    make: 'morph',
+    points: {
+      step0: points1,
+      step1: points2,
+      step2: points3,
+      step3: points4,
+    },
+    color: { step0: colors1, step1: colors2, step2: colors3, step3: colors4 },
+    // color: [1, 0, 0, 1],
+  });
 
-// trace.animations.new()
-//   .custom(
-//     {
-//       callback: (p) => {
-//         trace.custom.updateUniform('u_from', 3);
-//         trace.custom.updateUniform('u_to', 2);
-//         trace.custom.updateUniform('u_percent', p);
-//       },
-//       duration: 2,
-//     },
-//   )
-//   .custom(
-//     {
-//       callback: (p) => {
-//         trace.custom.updateUniform('u_from', 2);
-//         trace.custom.updateUniform('u_to', 1);
-//         trace.custom.updateUniform('u_percent', p);
-//       },
-//       duration: 2,
-//     },
-//   )
-//   .custom(
-//     {
-//       callback: (p) => {
-//         trace.custom.updateUniform('u_from', 1);
-//         trace.custom.updateUniform('u_to', 0);
-//         trace.custom.updateUniform('u_percent', p);
-//       },
-//       duration: 2,
-//     },
-//   )
-//   .start();
-
-// figure.addFrameRate(10, { font: { color: [1, 1, 1, 0]}});
-// figure.animateNextFrame();
-
-const m = figure.add({
-  make: 'morph',
-  points: {
-    step0: points1,
-    step1: points2,
-    step2: points3,
-    step3: points4,
-  },
-  color: { step0: colors1, step1: colors2, step2: colors3, step3: colors4 },
-  // color: {
-  //   step0: [],
-  //   step1: [],
-  //   step2: [],
-  // },
-  // color: [1, 0, 0, 1],
-});
-
-m.animations.new()
-  .delay(1)
-  .morph({ start: 'step0', target: 'step1', duration: 1 })
-  .morph({ start: 'step1', target: 'step2', duration: 1 })
-  .morph({ start: 'step2', target: 'step3', duration: 1 })
-  .morph({ start: 'step3', target: 'step0', duration: 1 })
-  .start();
+  m.animations.new()
+    .delay(2)
+    .morph({ start: 'step0', target: 'step3', duration: 3 })
+    .delay(1)
+    .morph({ start: 'step3', target: 'step0', duration: 3 })
+    // .morph({ start: 'step1', target: 'step2', duration: 1 })
+    // .morph({ start: 'step2', target: 'step3', duration: 4 })
+    // .delay(2)
+    // .morph({ start: 'step3', target: 'step0', duration: 2 })
+    .start();
+}, false);
