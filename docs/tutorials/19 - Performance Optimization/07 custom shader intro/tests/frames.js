709ee40a267b68274e785199eb2789f0fac966bb
airladon <airladon@gmail.com>
2021-06-07 13:53:25 -0700

    update performance tutorial with tests

---
+++ docs/tutorials/19 - Performance Optimization/07 custom shader intro/tests/frames.js
@@ -0,0 +1,16 @@
+/* global __duration __frames __timeStep __title, __width, __height */
+/* eslint no-unused-vars: ["error", { "vars": "local" }] */
+/* eslint-disable no-global-assign */
+
+__title = 'Tutorial - 19 - Performance Optimization - 07 custom shader intro';
+__duration = 4;
+__timeStep = 1;
+__width = 500;
+__height = 500;
+__frames = [
+  [0, `
+    startTime = null;
+    figure.elements.__frameRate_.hide();
+  `],
+];
+
---
+++ docs/tutorials/19 - Performance Optimization/08 custom shader/tests/frames.js
@@ -0,0 +1,16 @@
+/* global __duration __frames __timeStep __title, __width, __height */
+/* eslint no-unused-vars: ["error", { "vars": "local" }] */
+/* eslint-disable no-global-assign */
+
+__title = 'Tutorial - 19 - Performance Optimization - 08 custom shader';
+__duration = 4;
+__timeStep = 1;
+__width = 500;
+__height = 500;
+__frames = [
+  [0, `
+    startTime = null;
+    figure.elements.__frameRate_.hide();
+  `],
+];
+
