ca81f43466ad6651b06e546eabe11fffc21e0faa
airladon <airladon@gmail.com>
2021-06-16 22:23:39 -0700

    cleanup

--- src/js/tools/mesh/lathe.js
+++ src/js/tools/mesh/lathe.js
@@ -4,21 +4,25 @@ import type { TypeParsablePoint } from '../geometry/Point';
 import { joinObjects } from '../tools';
 import { getNormal } from '../geometry/Plane';
 import { Transform } from '../geometry/Transform';
-import { getPolygonCorners } from '../morph';
 
 export type OBJ_LatheMesh = {
   sides?: number,
   profile?: Array<TypeParsablePoint>,
-  normals?: 'curved' | 'flat',
+  normals?: 'curved' | 'flat' | 'curved2',
   position?: TypeParsablePoint,
-  fillEnds?: 'start' | 'end' | boolean,
   rotation?: TypeRotationDefinition,
   latheRotation?: number,
-  // sidesToDraw?: number,
-  // direction?: 1 | -1,
 }
 
-export default function lathe (options: OBJ_LatheMesh) {
+/**
+ * Create a mesh from a lathe profile
+ *
+ * The normals can be:
+ * - flat: each triangle's vertex has the same normal
+ * - curved: normals are averaged around axis of rotation
+ * - curved2: normals are averaged around and along axis of rotation.
+ */
+export default function lathe(options: OBJ_LatheMesh) {
   const o = joinObjects(
     {
       sides: 10,
@@ -38,7 +42,7 @@ export default function lathe (options: OBJ_LatheMesh) {
   }
 
   const {
-    sides, profile, latheRotation, fillEnds, normals,
+    sides, profile, latheRotation, normals,
   } = o;
   const points = [];
   const dAngle = Math.PI * 2 / sides;
@@ -48,8 +52,8 @@ export default function lathe (options: OBJ_LatheMesh) {
     for (let j = 0; j < profile.length; j += 1) {
       profilePoints.push(getPoint([
         profile[j].x,
-        profile[j].y * Math.cos(dAngle * i + latheRotation),
-        profile[j].y * Math.sin(dAngle * i + latheRotation),
+        Math.max(profile[j].y, 0.00001) * Math.cos(dAngle * i + latheRotation),
+        Math.max(profile[j].y, 0.00001) * Math.sin(dAngle * i + latheRotation),
       ]).transformBy(matrix).add(o.position));
     }
     points.push(profilePoints);
@@ -58,18 +62,18 @@ export default function lathe (options: OBJ_LatheMesh) {
   const triangles = [];
   const norms = [];
   const quadNorms = [];
-  const quadNorms1 = [];
   const qNorms = [];
-  const triNorms = [];
-  for (let i = 0; i < sides + 1; i += 1) {
-    const q = [];
-    const q1 = [];
-    for (let j = 0; j < profile.length; j += 1) {
-      q.push([]);
-      q1.push([]);
+  if (normals === 'curved2') {
+    for (let i = 0; i < sides + 1; i += 1) {
+      const q = [];
+      const q1 = [];
+      for (let j = 0; j < profile.length; j += 1) {
+        q.push([]);
+        q1.push([]);
+      }
+      quadNorms.push(q);
+      qNorms.push(q1);
     }
-    quadNorms.push(q);
-    qNorms.push(q1);
   }
 
   for (let i = 0; i < sides; i += 1) {
@@ -81,7 +85,7 @@ export default function lathe (options: OBJ_LatheMesh) {
       triangles.push(...a1.toArray(), ...b2.toArray(), ...a2.toArray());
       triangles.push(...a1.toArray(), ...b1.toArray(), ...b2.toArray());
       const norm = getNormal(a1, b1, a2);
-      if (normals === 'curvedAll') {
+      if (normals === 'curved2') {
         quadNorms[i][j].push(norm);
         quadNorms[i][j + 1].push(norm);
         quadNorms[i + 1][j].push(norm);
@@ -111,7 +115,7 @@ export default function lathe (options: OBJ_LatheMesh) {
     }
   }
 
-  if (normals === 'curvedAll') {
+  if (normals === 'curved2') {
     for (let i = 0; i < sides + 1; i += 1) {
       for (let j = 0; j < profile.length; j += 1) {
         let n = quadNorms[i][j][0];
@@ -146,61 +150,6 @@ export default function lathe (options: OBJ_LatheMesh) {
       }
     }
   }
-  console.log(triangles.length)
-  // const end1 = getPolygonCorners({ radius: profile[0].y, sides, rotation: latheRotation }).map(p => new Point(0, p.x, -p.y));
-  // const end2 = getPolygonCorners({ radius: profile.slice(-1)[0].y, sides, rotation: latheRotation }).map(p => new Point(0, p.x, -p.y));;
-  // // const t = new Fig.Transform().rotate(rx, ry, rz);
-  // // const tc = end1.map(c => c.transformBy(t.mat));
-  // // const tcZ = end2.map(c => c.transformBy(t.mat));
-  // const frontNormal = new Fig.Point(0, 0, -1).transformBy(t.mat);
-  // const backNormal = new Fig.Point(0, 0, 1).transformBy(t.mat);
-  // const sideNormals = [];
-  // const delta = Math.PI * 2 / sides / 2;
-  // for (let i = 0; i < sides; i += 1) {
-  //   const angle = delta + i * delta * 2;
-  //   const normal = Fig.tools.g2.polarToRect(1, angle);
-  //   sideNormals.push(normal.transformBy(t.mat));
-  // }
-  // const points = [];
-  // const normals = [];
-  // for (let i = 0; i < sides; i += 1) {
-  //   const next = i === corners.length - 1 ? 0 : i + 1;
-  //   points.push(new Fig.Point(0, 0, 0).transformBy(t.mat));
-  //   points.push(tc[i]._dup());
-  //   points.push(tc[next]._dup());
-  //   normals.push(frontNormal._dup());
-  //   normals.push(frontNormal._dup());
-  //   normals.push(frontNormal._dup());
-  // }
-  // for (let i = 0; i < corners.length; i += 1) {
-  //   const next = i === corners.length - 1 ? 0 : i + 1;
-  //   points.push(tc[i]._dup(), tcZ[i]._dup(), tcZ[next]._dup());
-  //   points.push(tc[i]._dup(), tcZ[next]._dup(), tc[next]._dup());
-  //   normals.push(sideNormals[i]._dup());
-  //   normals.push(sideNormals[i]._dup());
-  //   normals.push(sideNormals[i]._dup());
-  //   normals.push(sideNormals[i]._dup());
-  //   normals.push(sideNormals[i]._dup());
-  //   normals.push(sideNormals[i]._dup());
-  // }
-  // for (let i = 0; i < sides; i += 1) {
-  //   const next = i === corners.length - 1 ? 0 : i + 1;
-  //   points.push(new Fig.Point(0, 0, length).transformBy(t.mat));
-  //   points.push(tcZ[i]);
-  //   points.push(tcZ[next]);
-  //   normals.push(backNormal._dup());
-  //   normals.push(backNormal._dup());
-  //   normals.push(backNormal._dup());
-  // }
-  // const vertices = [];
-  // const norms = [];
-  // for (let i = 0; i < points.length; i += 1) {
-  //   vertices.push(points[i].x);
-  //   vertices.push(points[i].y);
-  //   vertices.push(points[i].z);
-  //   norms.push(normals[i].x);
-  //   norms.push(normals[i].y);
-  //   norms.push(normals[i].z);
-  // }
+  
   return [triangles, norms];
 }
