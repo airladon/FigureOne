ab4f1485bca30623c8ddaae61714dcdbd3c58fba
airladon <airladon@gmail.com>
2021-09-02 16:48:30 -0700

    add simple quaternion

--- scratch/simple2/index.js
+++ scratch/simple2/index.js
@@ -224,6 +224,48 @@ const pan = figure.add({
   move: true,
   color: [1, 0, 0, 0.2],
 });
+
+const quaternion = (a, b, c, d) => ({ a, b, c, d });
+const pointToQaternion = p => {
+  const pp = Fig.getPoint(p);
+  return { a: 0, b: pp.x, c: pp.y, d: pp.z };
+}
+const angleAxisToQuaternion = (angle, x, y, z) => {
+  const a = Math.cos(angle / 2);
+  const u = Fig.getPoint([x, y, z]).normalize().scale(Math.sin(angle / 2));
+  const b = u.x;
+  const c = u.y;
+  const d = u.z;
+  return { a, b, c, d };
+}
+
+const qMul = (q1, q2) => {
+  const a = q1.a * q2.a - q1.b * q2.b - q1.c * q2.c - q1.d * q2.d;
+  const b = q1.a * q2.b + q1.b * q2.a + q1.c * q2.d - q1.d * q2.c;
+  const c = q1.a * q2.c - q1.b * q2.d + q1.c * q2.a + q1.d * q2.b;
+  const d = q1.a * q2.d + q1.b * q2.c - q1.c * q2.b + q1.d * q2.a;
+  return { a, b, c, d };
+}
+
+const inv = (q) => {
+  return { a: q.a, b: -q.b, c: -q.c, d: -q.d };
+}
+
+const transformPoint = (p, q) => {
+  const pq = pointToQaternion(p);
+  const qInv = inv(q);
+  const p1 = qMul(q, qMul(pq, qInv));
+  return Fig.getPoint([p1.b, p1.c, p1.d]);
+}
+
+const dt = (p, angle1, axis1, angle2, axis2) => {
+  const q1 = angleAxisToQuaternion(angle1, ...axis1.toArray());
+  const q2 = angleAxisToQuaternion(angle2, ...axis2.toArray());
+  return transformPoint(p, qMul(q1, q2));
+  // return transformPoint(p, q1);
+}
+// console.log(transformPoint([1, 0, 0], angleAxisToQuaternion(Math.PI / 2, 0, 0, 1)));
+
 // pan.custom.last = pan.getPosition();
 pan.notifications.add('setTransform', () => {
   const delta = pan.getPosition();
@@ -232,6 +274,10 @@ pan.notifications.add('setTransform', () => {
   const lookAt = Fig.getPoint(figure.scene.camera.lookAt);
   const up = Fig.getPoint(figure.scene.camera.up);
   const position = Fig.getPoint(figure.scene.camera.position);
+
+  let deltaAz = deltaX;
+  let deltaEl = deltaY;
+
 
   // Method 1
   // const panAxis = lookAt.sub(position).crossProduct(up.crossProduct(lookAt.sub(position)));
@@ -245,14 +291,16 @@ pan.notifications.add('setTransform', () => {
   // Method 2
   const verticalAxis = Fig.getPoint([0, 1, 0]);
   const panAxis = verticalAxis;
-  const tiltAxis = lookAt.sub(position).crossProduct(up);
+  const tiltAxis = lookAt.sub(position).crossProduct(up).normalize();
+  // const tiltAxis = verticalAxis.crossProduct(position.sub(lookAt));
+  // const tiltAxis = verticalAxis.crossProduct([position.x, 0, position.z]);
   // const angleToLock = position.sub(lookAt).normalize().dotProduct(lookAt.add(verticalAxis).normalize());
   // console.log(angleToLock)
   // if (angleToLock > 0.999 && deltaY < 0) {
   //   deltaY = 0;
   // }
   // console.log(angleToLock);
-  let deltaAngle = 0.001;
+  let deltaAngle = 0.05;
   let angleToLock = Math.abs(Math.acos(
     position.sub(lookAt).normalize()
       .dotProduct(lookAt.add(verticalAxis).normalize()),
@@ -262,7 +310,7 @@ pan.notifications.add('setTransform', () => {
   // }
   
   // console.log(angleToLock)
-  if (deltaY < 0 && angleToLock < Math.PI / 2) {
+  if (deltaEl < 0 && angleToLock < Math.PI / 2) {
     // let angleToLock = Math.abs(Math.acos(
     //   position.sub(lookAt).normalize()
     //     .dotProduct(lookAt.add(verticalAxis).normalize()),
@@ -270,13 +318,13 @@ pan.notifications.add('setTransform', () => {
     // if (angleToLock > Math.PI / 2) {
     //   angleToLock = Math.PI - angleToLock;
     // }
-    if (deltaY < -angleToLock + deltaAngle) {
-      deltaY = -angleToLock + deltaAngle + 0.001;
+    if (deltaEl < -angleToLock + deltaAngle) {
+      deltaEl = -angleToLock + deltaAngle;
     }
-    // deltaY = Fig.tools.math.round(Math.max(-angleToLock + deltaAngle, deltaY), 4);
-    // console.log(angleToLock, deltaY)
+    // deltaEl = Fig.tools.math.round(Math.max(-angleToLock + deltaAngle, deltaEl), 4);
+    // console.log(angleToLock, deltaEl)
   }
-  if (deltaY > 0 && angleToLock > Math.PI / 2) {
+  if (deltaEl > 0 && angleToLock > Math.PI / 2) {
     // let angleToLock = Math.abs(Math.acos(
     //   position.sub(lookAt).normalize()
     //     .dotProduct(lookAt.add(verticalAxis).normalize()),
@@ -284,19 +332,26 @@ pan.notifications.add('setTransform', () => {
     // if (angleToLock > Math.PI / 2) {
       angleToLock = Math.PI - angleToLock;
     // }
-    deltaY = Fig.tools.math.round(Math.min(angleToLock - deltaAngle, deltaY), 4);
+    deltaEl = Fig.tools.math.round(Math.min(angleToLock - deltaAngle, deltaEl), 4);
   }
-  const t = [['r', deltaX, ...panAxis.toArray()]];
-  if (deltaY > 0.0001 || deltaY < -0.0001) {
-    t.push(['r', deltaY, ...tiltAxis.toArray()]);
-  }
+  
+  const t = [['r', deltaAz, ...panAxis.toArray()], ['r', deltaEl, ...tiltAxis.toArray()]];
+  // if (deltaEl > 0.0001 || deltaEl < -0.0001) {
+  //   t.push(['r', deltaY, ...tiltAxis.toArray()]);
+  // }
   // console.log(deltaX, deltaY)
   // const matrix = new Fig.Transform().rotate(deltaX, panAxis).rotate(deltaY, tiltAxis).matrix();
   const matrix = Fig.getTransform(t).matrix();
-  const newPosition = position.transformBy(matrix);
+  // const newPosition = position.transformBy(matrix);
+  const newPosition = dt(position, deltaEl, tiltAxis, deltaAz, panAxis);
+
+
+  // console.log('before', tiltAxis.round(2).toArray(), position.round(5).toArray())
 
   figure.scene.setCamera({
     position: newPosition,
   });
   pan.transform.updateTranslation([0, 0, 0]);
+
+  // console.log('after', lookAt.sub(Fig.getPoint(figure.scene.camera.position)).crossProduct(up).normalize().round(2).toArray(), newPosition.round(5).toArray())
 });
