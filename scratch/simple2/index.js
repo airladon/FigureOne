1adea68a9bb08b733ed92972392c8aec8c49aa76
airladon <airladon@gmail.com>
2021-09-20 12:21:23 -0700

    update version number

--- scratch/simple2/index.js
+++ scratch/simple2/index.js
@@ -1,140 +1,121 @@
-
-
-// const figure = new Fig.Figure();
-// figure.scene.setProjection({ style: 'orthographic' });
-// figure.scene.setCamera({ position: [2, 1, 1], up: [0, 1, 0] });
-// figure.scene.setLight({ directional: [0.1, 0.3, 1], ambient: 0.1 });
+// // Tutorial 20 - 3D
+// const figure = new Fig.Figure({ scene: { style: 'orthographic' } });
 
 // figure.add([
-//   // {
-//   //   make: 'cylinder',
-//   //   radius: 0.01,
-//   //   color: [1, 0, 0, 1],
-//   //   line: [[-1, 0, 0], [1, 0, 0]],
-//   // },
-//   // {
-//   //   make: 'cylinder',
-//   //   radius: 0.01,
-//   //   color: [0, 1, 0, 1],
-//   //   line: [[0, -1, 0], [0, 1, 0]],
-//   // },
-//   // {
-//   //   make: 'cylinder',
-//   //   radius: 0.01,
-//   //   color: [0, 0, 1, 1],
-//   //   line: [[0, 0, -1], [0, 0, 1]],
-//   // },
-//   // {
-//   //   make: 'grid',
-//   //   bounds: [-0.8, -0.8, 1.6, 1.6],
-//   //   xStep: 0.05,
-//   //   yStep: 0.05,
-//   //   line: { width: 0.002 },
-//   //   color: [0.7, 0.7, 0.7, 1],
-//   //   transform: ['r', Math.PI / 2, 1, 0, 0],
-//   // },
-// ]);
-
-
-// // const figure = new Fig.Figure({
-// //   scene: {
-// //     style: 'orthographic',
-// //     left: -2,
-// //     right: 2,
-// //     bottom: -1,
-// //     top: 1,
-// //     camera: {
-// //       position: [2, 1, 1],
-// //       up: [0, 1, 0],
-// //     },
-// //     light: {
-// //       directional: [1, 0.5, -0.1],
-// //     },
-// //   },
-// // });
-
-// // const figure = new Fig.Figure();
-// // figure.scene.setProjection({ style: 'orthographic' });
-// // figure.scene.setCamera({ position: [2, 1, 1], up: [0, 1, 0] });
-// // // Setup the direction of directional light. Note, this vector describes where
-// // // the light is coming from.
-// // figure.scene.setLight({ directional: [1, 0.5, -0.1] });
-// // const points = Fig.tools.g2.surfaceGrid({
-// //   x: [-0.8, 0.7, 0.03],
-// //   y: [-0.8, 0.7, 0.03],
-// //   z: x => 0.2 * Math.cos(x * 2 * Math.PI),
-// // });
-// // figure.scene.setCamera({ up: [0, 0, 1] });
-// figure.add({
-//   make: 'revolve',
-//   profile: [[0, 0.3], [0.5, 0.2], [1, 0.3], [1, 0.29], [0.5, 0.19], [0, 0.29], [0, 0.3]],
-//   color: [1, 0, 0, 1],
-//   sides: 30,
-// });
-
-// figure.add([
+//   // Add x, y, z axes
+//   {
+//     make: 'collections.axis3',
+//     start: -0.8,
+//     length: 1.6,
+//     arrow: { ends: 'all' },
+//     width: 0.01,
+//   },
+//   // Add a cube at the origin
+//   {
+//     make: 'cube',
+//     side: 0.3,
+//     color: [1, 0, 0, 1],
+//   },
+//   // Allow user to rotate the scene
 //   {
 //     make: 'cameraControl',
 //   },
 // ]);
 
-// //  const t1 = new Fig.Transform().scale(2).rotate(Math.PI / 2).translate(1, 1);
-// // const t2 = new Fig.Transform([['s', 2], ['r', Math.PI / 2], ['t', 1, 1]]);
-// // const t3 = Fig.getTransform([['s', 2], ['r', Math.PI / 2], ['t', 1, 1]]);
-// // console.log(t1.isEqualTo(t2))
-// // console.log(t1.isEqualTo(t3))
 
-// const figure = new Fig.Figure({ scene: [0, 0, 6, 3]});
-// const figure = new Fig.Figure({ scene: { style: 'orthographic' } });
-// // figure.add(
-// //   {
-// //     make: 'polygon',
-// //     sides: 8,
-// //     radius: 0.2,
-// //     // create a touch border that is a rectangle around the border
-// //     // with a buffer of 0.1 on the left and right, and 0.3 on bottom
-// //     // and top
-// //     touchBorder: [0.1, 0.3],
-// //     color: [1, 0, 0, 1],
-// //   },
-// // );
-// // figure.showTouchBorders();
+// // Tutorial 21 - 3D Surface
+// const { Figure, surfaceGrid } = Fig;
 
-// figure.add(
-//   {
-//     name: 'c',
-//     make: 'collection',
-//     elements: [
-//       {
-//         name: 'cube',
-//         make: 'cube',
-//         color: [1, 1, 0, 1],
-//         side: 0.4,
-//       },
-//       {
-//         name: 'sphere',
-//         make: 'sphere',
-//         position: [0.8, 0, 0],
-//         color: [1, 1, 0, 1],
-//         radius: 0.2,
-//       },
-//     ],
-//     touch: {
-//       onClick: () => console.log('a'),
-//     },
-//     touchScale: 2,
+// // Orthographic scene with camera oriented so z is up
+// const figure = new Figure({
+//   scene: {
+//     style: 'orthographic',
+//     camera: { up: [0, 0, 1] },
 //   },
-// );
-// figure.showTouchable();
+// });
+
+// // Surface with wire mesh and fill
+// const points = surfaceGrid({
+//   x: [-0.8, 0.8, 0.02],
+//   y: [-0.8, 0.8, 0.02],
+//   z: (x, y) => {
+//     const r = Math.sqrt(x * x + y * y) * Math.PI * 2 * 2;
+//     return 0.9 * Math.sin(r) / r;
+//   },
+// });
+
+// // Bug in Scene where this needs to be here - fix BUG!!!
+// figure.add([
+//   {
+//     make: 'surface',
+//     points,
+//     color: [1, 0, 0, 1],
+//     normals: 'curve',
+//   },
+//   {
+//     make: 'surface',
+//     points,
+//     lines: true,
+//     color: [0, 0, 0, 1],
+//     position: [0, 0, 0.001],
+//   },
+//   { make: 'cameraControl', axis: [0, 0, 1] },
+// ]);
 
 
-const figure = new Fig.Figure();
-figure.add(
-  {
-    make: 'polygon',
-    sides: 8,
-    radius: 0.2,
-    move: true,
-    color: [1, 0, 0, 1],
+
+// Tutorial 21 - 3D - Interactive cube
+const { Figure, round } = Fig;
+const figure = new Figure({ scene: { style: 'orthographic' } });
+
+// Top grid
+figure.add({
+  name: 'topGrid',
+  make: 'grid',
+  step: 0.2,
+  transform: ['r', Math.PI / 2, 1, 0, 0],
+  width: 0.005,
+  color: [1, 0.4, 0.4, 1],
+  bounds: [-1.1, -1.1, 2.2, 2.2],
+});
+
+// Bottom grid
+figure.add({
+  make: 'grid',
+  step: 0.2,
+  transform: [['r', Math.PI / 2, 1, 0, 0], ['t', 0, -0.7, 0]],
+  width: 0.005,
+  color: [0.5, 0.5, 0.5, 1],
+  bounds: [-1.1, -1.1, 2.2, 2.2],
+});
+
+const shaddow = figure.add({
+  make: 'rectangle',
+  width: 0.2,
+  height: 0.2,
+  color: [0, 0, 0, 0.5],
+  transform: [['r', Math.PI / 2, 1, 0, 0], ['t', 0, -0.7, 0]],
+});
+
+const cube = figure.add({
+  make: 'cube',
+  side: 0.2,
+  position: [0, 0.1, 0],
+  color: [1, 0, 0, 1],
+  move: {
+    plane: [[0, 0, 0.1], [0, 1, 0]],
+    bounds: {
+      left: -1, right: 1, bottom: -1, top: 1, position: [0, 0.1, 0], normal: [0, 1, 0],
+    },
   },
-);
+});
+
+cube.notifications.add('setTransform', (t) => {
+  const p = t[0].t();
+  shaddow.setPosition([
+    round(p.x * 5, 0) / 5, -0.7, round(p.z * 5, 0) / 5,
+  ]);
+});
+
+
+
