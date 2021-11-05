e9f24e35752d535011987783fb846271d58424cf
airladon <airladon@gmail.com>
2021-11-04 17:33:31 -0700

    finish txt tests

--- src/tests/primitives/txt/index2d.js
+++ src/tests/primitives/txt/index2d.js
@@ -92,7 +92,7 @@ const txt = (name, options, lineOptions = null) => makeShape(
   tools.misc.joinObjects({}, {
     color: [1, 0, 0, 1],
     touchBorder: 'buffer',
-    type: 'bmp',
+    type: '2d',
   }, options),
   lineOptions,
 );
--- src/tests/primitives/txt/text.btest.js
+++ src/tests/primitives/txt/text.btest.js
@@ -3,10 +3,18 @@ const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
 expect.extend({ toMatchImageSnapshot });
 
-test('Primitives: Text', async () => {
-  console.log(`http://localhost:8080/${__dirname.replace('/home/pwusr/', '')}/index.html`)
+test('Primitives: Text GL', async () => {
   // eslint-disable-next-line no-undef
-  await page.goto(`http://localhost:8080/${__dirname.replace('/home/pwusr/', '')}/index.html`);
+  await page.goto(`http://localhost:8080${__dirname.replace('/home/pwuser', '')}/index.html`);
+  // eslint-disable-next-line no-undef
+  const image = await page.screenshot({ fullPage: true });
+  expect(image).toMatchImageSnapshot();
+});
+
+
+test('Primitives: Text 2D', async () => {
+  // eslint-disable-next-line no-undef
+  await page.goto(`http://localhost:8080${__dirname.replace('/home/pwuser', '')}/index2d.html`);
   // eslint-disable-next-line no-undef
   const image = await page.screenshot({ fullPage: true });
   expect(image).toMatchImageSnapshot();
