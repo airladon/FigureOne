b2d9aad4220faa6834b5e8b9659703590c442684
airladon <airladon@gmail.com>
2021-12-04 19:36:36 -0800

    update snapshots

--- src/tests/collections/equation/static.test.js
+++
@@ -1,7 +0,0 @@
-import { staticTester } from '../../staticTester';
-
-const {
-  getShapes, updates, getValues, move,
-} = require('./static');
-
-staticTester('Collection: Angle - Static', getShapes, updates, getValues, move);
