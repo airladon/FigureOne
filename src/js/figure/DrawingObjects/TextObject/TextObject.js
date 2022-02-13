b11c5565959ee163a4cd579b07392cfb7f025866
airladon <airladon@gmail.com>
2022-02-12 22:21:54 -0800

    add not equals unicode char to latin charset

--- src/js/figure/DrawingObjects/TextObject/TextObject.js
+++ src/js/figure/DrawingObjects/TextObject/TextObject.js
@@ -30,7 +30,7 @@ import glyphMeasures from './glyphMeasures';
 const greek = '\u0391\u0392\u0393\u0394\u0395\u0396\u0397\u0398\u0399\u039A\u039B\u039C\u039D\u039E\u039F\u03A0\u03A1\u03A3\u03A4\u03A5\u03A6\u03A7\u03A8\u03A9\u03B1\u03B2\u03B3\u03B4\u03B5\u03B6\u03B7\u03B8\u03B9\u03BA\u03BB\u03BC\u03BD\u03BE\u03BF\u03C0\u03C1\u03C2\u03C3\u03C4\u03C5\u03C6\u03C7\u03C8\u03c9gh';
 
 // eslint-disable-next-line quotes
-const latin = `QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm,./<>?;':"[]\\{}|1234567890!@#$%^&*()-=_+" `;
+const latin = `QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm,./<>?;':"[]\\{}|1234567890!@#$%^&*()-=_+≠" `;
 
 // const math = '\u00ba\u00b0\u00d7\u00f7';
 
