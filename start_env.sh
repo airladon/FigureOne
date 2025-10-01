d8e6757dd9b7eb72edd4b59514d0f1cc5b9975dd
airladon <airladon@gmail.com>
2025-09-30 21:15:00 -0700

    enable git in container

--- start_env.sh
+++ start_env.sh
@@ -12,16 +12,24 @@ docker run -it --rm \
   -v $PROJECT_PATH/tools:/opt/app/tools \
   -v $PROJECT_PATH/containers/figureone/webpack.config.js:/opt/app/webpack.config.js \
   -v $PROJECT_PATH/build.sh:/opt/app/build.sh \
-  -v $PROJECT_PATH/containers/figureone/generate_docs.sh:/opt/app/generate_docs.sh \
-  -v $PROJECT_PATH/containers/figureone/browser.sh:/opt/app/browser.sh \
+  -v $PROJECT_PATH/containers/generate_docs.sh:/opt/app/generate_docs.sh \
+  -v $PROJECT_PATH/containers/browser.sh:/opt/app/browser.sh \
   -v $PROJECT_PATH/.eslintrc.json:/opt/app/.eslintrc.json \
   -v $PROJECT_PATH/.dockerignore:/opt/app/.dockerignore \
   -v $PROJECT_PATH/.eslintignore:/opt/app/.eslintignore \
   -v $PROJECT_PATH/.flowconfig:/opt/app/.flowconfig \
   -v $PROJECT_PATH/.babelrc:/opt/app/.babelrc \
+  -v $PROJECT_PATH/.git:/opt/app/.git \
+  -v $PROJECT_PATH/.gitignore:/opt/app/.gitignore \
   -v $PROJECT_PATH/documentation.yml:/opt/app/documentation.yml \
   -v $PROJECT_PATH/jest.config.js:/opt/app/jest.config.js \
   -v $PROJECT_PATH/jest.perf.config.js:/opt/app/jest.perf.config.js \
+  -v $PROJECT_PATH/README.md:/opt/app/README.md \
+  -v $PROJECT_PATH/global.d.ts:/opt/app/global.d.ts \
+  -v $PROJECT_PATH/tsconfig.json:/opt/app/tsconfig.json \
+  -v $PROJECT_PATH/start_env.sh:/opt/app/start_env.sh \
+  -v $PROJECT_PATH/dev.sh:/opt/app/dev.sh \
+  -v $PROJECT_PATH/LICENSE:/opt/app/LICENSE \
   -v $PROJECT_PATH/docs:/opt/app/docs \
   -v $PROJECT_PATH/reports:/opt/app/reports \
   -v $PROJECT_PATH/jsdoc-conf.json:/opt/app/jsdoc-conf.json \
