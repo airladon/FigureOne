1e30afc625411b9da5d8cd3efc8923f94b6d3661
airladon <airladon@gmail.com>
2026-02-15 16:23:13 -0800

    Next migration

--- src/js/figure/FigurePrimitives/FigurePrimitiveTypes.d.ts
+++
@@ -1,7 +0,0 @@
-export type TypeGLPrimitive = 'TRIANGLES' | 'POINTS' | 'FAN' | 'STRIP' | 'LINES';
-
-export type OBJ_Touch = {
-  onClick?: string | ((p: any, element: any) => void);
-  colorSeed?: string;
-  enable?: boolean;
-};
