14c744c467acea1ac2e627a30407f98e8fc5113e
airladon <airladon@gmail.com>
2026-04-21 15:33:55 -0700

    fix snapshot bug

--- src/tests/api-examples/primitives3d.api.btest.ts
+++ src/tests/api-examples/primitives3d.api.btest.ts
@@ -8,4 +8,10 @@ figure.scene.setProjection({ style: 'orthographic' });
 figure.scene.setCamera({ position: [2, 1, 1], up: [0, 1, 0] });
 figure.scene.setLight({ directional: [0.7, 0.5, 1] });
 `,
+  0,
+  `
+if (Object.values(figure.webglLow.textures).some(t => t.state === 'loading')) {
+  sleepTime = 1000;
+}
+`,
 );
